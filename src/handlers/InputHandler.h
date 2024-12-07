a39a42eaa79a471261c9bb0b5c1fbe66a3d068ee
rn-santos227 <rn_santos227@yahoo.com>
2024-12-08 00:34:46 +0800

    added input actions enum in the input handler header file

--- src/handlers/InputHandler.h
+++ src/handlers/InputHandler.h
@@ -2,6 +2,20 @@
 #define INPUT_HANDLER_H
 
 #include <SDL2/SDL.h>
-#include <vector>
+#include <unordered_map>
+
+enum class InputAction {
+  NONE,
+  UP, 
+  DOWN,
+  LEFT,
+  RIGHT,
+  ACTION_1,
+  ACTION_2,
+  ESCAPE,
+  ENTER,
+  DEBUG_TOGGLE,
+  IMMEDIATE_EXIT,
+}
 
 #endif 

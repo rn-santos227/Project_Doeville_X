ace15075264c5ce604dc18808aa57917f1f4d96a
rn-santos227 <rn_santos227@yahoo.com>
2024-12-07 19:36:54 +0800

    updated main function in main file

--- src/main.cpp
+++ src/main.cpp
@@ -2,4 +2,11 @@
 
 int main() {
   std::cout << "Game Engine Initialized!" << std::endl;
+
+  while(true) {
+    std::cout << "Running game loop..." << std::endl;
+
+    //temporary breaks the loop
+    break;
+  }
 }

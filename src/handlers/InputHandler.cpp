4bc4fc80e2ab824dca2fdb3a308cd767b473ae08
rn-santos227 <rn_santos227@yahoo.com>
2024-12-08 01:10:31 +0800

    updated input handler constructor in input handler source

--- src/handlers/InputHandler.cpp
+++ src/handlers/InputHandler.cpp
@@ -1,9 +1,19 @@
 #include "InputHandler.h"
 #include <SDL2/SDL.h>
-#include <iostream>
 #include <unordered_map>
+#include <functional>
+#include <iostream>
 
-InputHandler::InputHandler() : currentAction(InputAction::NONE) {}
+InputHandler::InputHandler() {
+  keyBindings[InputAction::UP] = SDL_SCANCODE_W;
+  keyBindings[InputAction::DOWN] = SDL_SCANCODE_S;
+  keyBindings[InputAction::LEFT] = SDL_SCANCODE_A;
+  keyBindings[InputAction::RIGHT] = SDL_SCANCODE_D;
+  keyBindings[InputAction::ESCAPE] = SDL_SCANCODE_ESCAPE;
+  keyBindings[InputAction::ENTER] = SDL_SCANCODE_RETURN;
+  keyBindings[InputAction::ACTION_1] = SDL_SCANCODE_Z;
+  keyBindings[InputAction::ACTION_2] = SDL_SCANCODE_Y;
+}
 
 InputHandler::~InputHandler() {}
 

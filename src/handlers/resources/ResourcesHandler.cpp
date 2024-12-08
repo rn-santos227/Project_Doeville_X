a0cacd37c5e50207a2c7fa39ea205fa835924d77
rn-santos227 <rn_santos227@yahoo.com>
2024-12-09 01:10:00 +0800

    added get base path funciton in resoucres handler source file

--- src/handlers/resources/ResourcesHandler.cpp
+++ src/handlers/resources/ResourcesHandler.cpp
@@ -1,1 +1,11 @@
 #include "ResourcesHandler.h"
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <cstdlib> 
+
+std::string ResourcesHandler::getBasePath() {
+  char* basePath = SDL_GetBasePath();
+  std::string path = basePath ? basePath : "";
+  SDL_free(basePath);
+  return path;
+}

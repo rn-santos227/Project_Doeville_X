3601a0fefedf597c7463460718947a2768dff5b2
rn-santos227 <rn_santos227@yahoo.com>
2024-12-11 00:11:40 +0800

    updated input action header file

--- src/handlers/input/InputAction.h
+++ src/handlers/input/InputAction.h
@@ -1,3 +1,21 @@
 #ifndef INPUT_ACTION_H
+#define INPUT_ACTION_H
+
+enum class InputAction {
+  NONE,
+  UP, 
+  DOWN,
+  LEFT,
+  RIGHT,
+  ACTION_1,
+  ACTION_2,
+  ESCAPE,
+  ENTER,
+  HELP_TOGGLE,
+  DEBUG_TOGGLE,
+  FREEZE_GAME,
+  RESTART_GAME,
+  IMMEDIATE_EXIT,
+};
 
 #endif

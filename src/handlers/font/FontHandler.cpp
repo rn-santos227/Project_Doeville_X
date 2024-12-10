23325399f2578f6cbadf20b293da8b85dbf11ef4
rn-santos227 <rn_santos227@yahoo.com>
2024-12-10 20:37:36 +0800

    updated render text function in font handler

--- src/handlers/font/FontHandler.cpp
+++ src/handlers/font/FontHandler.cpp
@@ -23,21 +23,21 @@ bool FontHandler::loadFont(const std::string& fontId, const std::string& filePat
 
 SDL_Texture* FontHandler::renderText(SDL_Renderer* renderer, const std::string& text, const std::string& fontId, SDL_Color color) {
   auto it = fonts.find(fontId);
-  if (logsManager.checkAndLogError(it == fonts.end(), "Font ID \"" + fontId + "\" not found!", nullptr)) {
+  if (logsManager.checkAndLogError(it == fonts.end(), "Font ID \"" + fontId + "\" not found!")) {
     return nullptr;
   }
 
   TTF_Font* font = it->second;
   SDL_Surface* textSurface = TTF_RenderText_Solid(font, text.c_str(), color);
 
-  if (logsManager.checkAndLogError(!textSurface, "Failed to create text surface: " + std::string(TTF_GetError()), nullptr)) {
+  if (logsManager.checkAndLogError(!textSurface, "Failed to create text surface: " + std::string(TTF_GetError()))) {
     return nullptr;
   }
 
   SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
   SDL_FreeSurface(textSurface);
 
-  logsManager.checkAndLogError(!textTexture, "Failed to create texture from text: " + std::string(SDL_GetError()), nullptr);
+  logsManager.checkAndLogError(!textTexture, "Failed to create texture from text: " + std::string(SDL_GetError()));
 
   return textTexture;
 }

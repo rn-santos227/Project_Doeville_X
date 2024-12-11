5a7d599b73e10f788cdf17827895daeba5c2c329
rn-santos227 <rn_santos227@yahoo.com>
2024-12-11 20:03:23 +0800

    updated handle event function in mouse handler

--- src/handlers/input/MouseHandler.cpp
+++ src/handlers/input/MouseHandler.cpp
@@ -2,3 +2,27 @@
 
 MouseHandler::MouseHandler(LogsManager& logsManager) : mouseX(0), mouseY(0), mouseWheelScroll(0), logsManager(logsManager) {}
 MouseHandler::~MouseHandler() {}
+
+void MouseHandler::handleEvent(const SDL_Event& event) {
+  switch (event.type) { 
+    case SDL_MOUSEBUTTONDOWN:
+      buttonStates[event.button.button] = true;
+      break;
+
+    case SDL_MOUSEBUTTONUP:
+      buttonStates[event.button.button] = false;
+      break;
+
+    case SDL_MOUSEMOTION:
+      mouseX = event.motion.x;
+      mouseY = event.motion.y;
+      break;
+
+    case SDL_MOUSEWHEEL:
+      mouseWheelScroll = event.wheel.y;
+      break;
+
+    default:
+      break;
+  }
+}

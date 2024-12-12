cffd8b20a86d08f001c83bc8a844f4a12b69c65b
rn-santos227 <rn_santos227@yahoo.com>
2024-12-13 00:10:12 +0800

    added add state function in gane state manager source

--- src/states/GameStateManager.cpp
+++ src/states/GameStateManager.cpp
@@ -1,5 +1,7 @@
 #include "GameStateManager.h";
 
 namespace Project::States {
-
+  void GameStateManager::addState(const std::string& name, std::unique_ptr<GameState> state) {
+    states[name] = std::move(state);
+  }
 }

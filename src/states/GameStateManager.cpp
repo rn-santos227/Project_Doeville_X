1603861078c709d861c4eea44084d916c7e3d307
rn-santos227 <rn_santos227@yahoo.com>
2024-12-13 00:11:20 +0800

    added change state function in gane state manager source

--- src/states/GameStateManager.cpp
+++ src/states/GameStateManager.cpp
@@ -4,4 +4,11 @@ namespace Project::States {
   void GameStateManager::addState(const std::string& name, std::unique_ptr<GameState> state) {
     states[name] = std::move(state);
   }
+
+  void GameStateManager::changeState(const std::string& name) {
+    if (!stateStack.empty()) {
+
+    }
+  }
 }
+

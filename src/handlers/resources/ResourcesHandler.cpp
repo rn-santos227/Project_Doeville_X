e00a3c54c86a530beb840c6624ac7f8d98dfe043
rn-santos227 <rn_santos227@yahoo.com>
2024-12-12 19:35:22 +0800

    implement namespace in resource handler

--- src/handlers/resources/ResourcesHandler.cpp
+++ src/handlers/resources/ResourcesHandler.cpp
@@ -1,23 +1,20 @@
 #include "ResourcesHandler.h"
 
-#include <cstdlib> 
-#include <iostream>
-
-#include <SDL.h>
-
-std::string ResourcesHandler::getBasePath() {
-  char* basePath = SDL_GetBasePath();
-  std::string path = basePath ? basePath : "";
-  SDL_free(basePath);
-  return path;
-}
-
-std::string ResourcesHandler::getResourcePath(const std::string& relativePath) {
-  std::string basePath = getBasePath();
-
-  if (basePath.back() == '\\') {
-    basePath.pop_back();
+namespace Project::Handlers {
+  std::string ResourcesHandler::getBasePath() {
+    char* basePath = SDL_GetBasePath();
+    std::string path = basePath ? basePath : "";
+    SDL_free(basePath);
+    return path;
   }
 
-  return basePath + "/" + relativePath;
+  std::string ResourcesHandler::getResourcePath(const std::string& relativePath) {
+    std::string basePath = getBasePath();
+
+    if (basePath.back() == '\\') {
+      basePath.pop_back();
+    }
+
+    return basePath + "/" + relativePath;
+  }
 }

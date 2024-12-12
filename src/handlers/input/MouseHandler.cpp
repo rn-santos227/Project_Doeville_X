76b26cac63b64b81ae0f9294ae763a7d5c952cd7
rn-santos227 <rn_santos227@yahoo.com>
2024-12-12 19:35:59 +0800

    implement namespace in mouse handler

--- src/handlers/input/MouseHandler.cpp
+++ src/handlers/input/MouseHandler.cpp
@@ -1,54 +1,56 @@
 #include "MouseHandler.h"
 
-MouseHandler::MouseHandler(LogsManager& logsManager) : mouseX(0), mouseY(0), mouseWheelScroll(0), logsManager(logsManager) {}
-MouseHandler::~MouseHandler() {}
+namespace Project::Handlers {
+  MouseHandler::MouseHandler(LogsManager& logsManager) : mouseX(0), mouseY(0), mouseWheelScroll(0), logsManager(logsManager) {}
+  MouseHandler::~MouseHandler() {}
 
-bool MouseHandler::isButtonDown(Uint8 button) const {
-  auto it = buttonStates.find(button);
-  return it != buttonStates.end() && it->second;
-}
-
-bool MouseHandler::isButtonUp(Uint8 button) const {
-  auto it = buttonStates.find(button);
-  return it == buttonStates.end() || !it->second;
-}
-
-int MouseHandler::getMouseX() const {
-  return mouseX;
-}
-
-int MouseHandler::getMouseY() const {
-  return mouseY;
-}
-
-int MouseHandler::getMouseWheelScroll() const {
-  return mouseWheelScroll;
-}
-
-void MouseHandler::handleEvent(const SDL_Event& event) {
-  switch (event.type) { 
-    case SDL_MOUSEBUTTONDOWN:
-      buttonStates[event.button.button] = true;
-      break;
-
-    case SDL_MOUSEBUTTONUP:
-      buttonStates[event.button.button] = false;
-      break;
-
-    case SDL_MOUSEMOTION:
-      mouseX = event.motion.x;
-      mouseY = event.motion.y;
-      break;
-
-    case SDL_MOUSEWHEEL:
-      mouseWheelScroll = event.wheel.y;
-      break;
-
-    default:
-      break;
+  bool MouseHandler::isButtonDown(Uint8 button) const {
+    auto it = buttonStates.find(button);
+    return it != buttonStates.end() && it->second;
   }
-}
 
-void MouseHandler::updateMousePosition() {
-  SDL_GetMouseState(&mouseX, &mouseY);
+  bool MouseHandler::isButtonUp(Uint8 button) const {
+    auto it = buttonStates.find(button);
+    return it == buttonStates.end() || !it->second;
+  }
+
+  int MouseHandler::getMouseX() const {
+    return mouseX;
+  }
+
+  int MouseHandler::getMouseY() const {
+    return mouseY;
+  }
+
+  int MouseHandler::getMouseWheelScroll() const {
+    return mouseWheelScroll;
+  }
+
+  void MouseHandler::handleEvent(const SDL_Event& event) {
+    switch (event.type) { 
+      case SDL_MOUSEBUTTONDOWN:
+        buttonStates[event.button.button] = true;
+        break;
+
+      case SDL_MOUSEBUTTONUP:
+        buttonStates[event.button.button] = false;
+        break;
+
+      case SDL_MOUSEMOTION:
+        mouseX = event.motion.x;
+        mouseY = event.motion.y;
+        break;
+
+      case SDL_MOUSEWHEEL:
+        mouseWheelScroll = event.wheel.y;
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  void MouseHandler::updateMousePosition() {
+    SDL_GetMouseState(&mouseX, &mouseY);
+  }
 }

92bc5a96358cf386ce8125b757a466d1589a9a28
rn-santos227 <rn_santos227@yahoo.com>
2024-12-12 20:18:29 +0800

    implement namespace in frame counter

--- src/utilities/frames_counter/FramesCounter.cpp
+++ src/utilities/frames_counter/FramesCounter.cpp
@@ -1,20 +1,22 @@
 #include "FramesCounter.h"
 
-FramesCounter::FramesCounter() : lastTime(SDL_GetPerformanceCounter()), frequency(SDL_GetPerformanceFrequency()), frameCount(0), fps(0) {}
+namespace Project::Utilities {
+  FramesCounter::FramesCounter() : lastTime(SDL_GetPerformanceCounter()), frequency(SDL_GetPerformanceFrequency()), frameCount(0), fps(0) {}
 
-void FramesCounter::update() {
-  frameCount++;
-  Uint64 currentTime = SDL_GetPerformanceCounter();
-  
-  double elapsedTime = (currentTime - lastTime) / (double)frequency * 1000;
+  void FramesCounter::update() {
+    frameCount++;
+    Uint64 currentTime = SDL_GetPerformanceCounter();
+    
+    double elapsedTime = (currentTime - lastTime) / (double)frequency * 1000;
 
-  if (elapsedTime >= 1000.0) {
-    fps = frameCount;
-    frameCount = 0;
-    lastTime = currentTime;
+    if (elapsedTime >= 1000.0) {
+      fps = frameCount;
+      frameCount = 0;
+      lastTime = currentTime;
+    }
   }
-}
 
-int FramesCounter::getFPS() const {
-  return fps;
+  int FramesCounter::getFPS() const {
+    return fps;
+  }
 }

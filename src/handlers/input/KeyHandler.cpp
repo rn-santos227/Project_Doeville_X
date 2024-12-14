c8e53383b1680a79070363c0df699b2c01e2d577
rn-santos227 <rn_santos227@yahoo.com>
2024-12-14 18:16:49 +0800

    updated key handler source file in inputs handler

--- src/handlers/input/KeyHandler.cpp
+++ src/handlers/input/KeyHandler.cpp
@@ -1,7 +1,7 @@
 #include "KeyHandler.h"
 
 namespace Project::Handlers {
-  KeyHandler::KeyHandler(Project::Utilities::LogsManager& logsManager) : logsManager(logsManager) {
+  KeyHandler::KeyHandler(LogsManager& logsManager) : logsManager(logsManager) {
     keyBindings[KeyAction::UP] = SDL_SCANCODE_W;
     keyBindings[KeyAction::DOWN] = SDL_SCANCODE_S;
     keyBindings[KeyAction::LEFT] = SDL_SCANCODE_A;

5196c485814d948c3f6f00cdfa9beff18070f8af
rn-santos227 <rn_santos227@yahoo.com>
2024-12-17 22:43:21 +0800

    added create state from lua function in game state factory source file

--- src/factories/state/GameStateFactory.cpp
+++ src/factories/state/GameStateFactory.cpp
@@ -3,5 +3,7 @@ namespace Project::Factories {
   GameStateFactory::GameStateFactory(GameStateManager& gameStateManager, LogsManager& logsManager)
   : gameStateManager(gameStateManager), logsManager(logsManager) {}
 
-  
+  bool GameStateFactory::createStateFromLua(const std::string& scriptPath) {
+
+  }
 }

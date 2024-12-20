171cd272520cac1e367b9dee753e16fb326ac841
rn-santos227 <rn_santos227@yahoo.com>
2024-12-20 22:52:01 +0800

    updated update function in game state source file

--- src/states/GameState.cpp
+++ src/states/GameState.cpp
@@ -51,7 +51,7 @@ namespace Project::States {
   void GameState::update(float deltaTime) {
     lua_getglobal(luaState, "update");
     if (lua_isfunction(luaState, -1)) {
-
+      int result = lua_pcall(luaState, 0, 0, 0);
     } else {
       lua_pop(luaState, 1);
     }

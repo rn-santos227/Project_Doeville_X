fbdae90d1caa68cd18b383f0d2d724d9ce8ddc55
rn-santos227 <rn_santos227@yahoo.com>
2024-12-25 20:31:57 +0800

    updated add entitiy function in entities manager source file

--- src/entities/EntitiesManager.cpp
+++ src/entities/EntitiesManager.cpp
@@ -2,6 +2,6 @@
 
 namespace Project::Entities {
   void EntitiesManager::addEntity(const std::string& id, std::shared_ptr<Entity> entity) {
-
+    std::lock_guard<std::mutex> lock(entitiesMutex);
   }
 }

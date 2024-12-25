d4b881b53558629c3ef17bcb4dd6667028ec751c
rn-santos227 <rn_santos227@yahoo.com>
2024-12-25 20:27:08 +0800

    added namespace in entities manager source file

--- src/entities/EntitiesManager.cpp
+++ src/entities/EntitiesManager.cpp
@@ -1,37 +1,5 @@
-#ifndef ENTITIES_MANAGER_H
-#define ENTITIES_MANAGER_H
-
-#include "Entity.h"
-
-#include <unordered_map>
-#include <vector>
-#include <memory>
-#include <string>
-#include <mutex>
+#include "EntitiesManager.h"
 
 namespace Project::Entities {
-  class EntitiesManager {
-    public:
-      explicit EntitiesManager(LogsManager& logsManager);
-      ~EntitiesManager();
 
-      void addEntity(const std::string& id, std::shared_ptr<Entity> entity);
-      void removeEntity(const std::string& id);
-
-      void unloadSceneEntities();
-      void optimizeEntities();
-
-      void update(float deltaTime);
-      void render();
-
-    private:
-      LogsManager& logsManager;
-
-      std::unordered_map<std::string, std::shared_ptr<Entity>> activeEntities;
-      std::unordered_map<std::string, std::shared_ptr<Entity>> cachedEntities;
-
-      std::mutex entitiesMutex;
-  };
 }
-
-#endif

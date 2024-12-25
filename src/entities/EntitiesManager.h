aa1a903b77b8d54b1f78bc89408e87d4a9a67d44
rn-santos227 <rn_santos227@yahoo.com>
2024-12-25 20:26:03 +0800

    updated entities manager header file

---
+++ src/entities/EntitiesManager.h
@@ -0,0 +1,37 @@
+#ifndef ENTITIES_MANAGER_H
+#define ENTITIES_MANAGER_H
+
+#include "Entity.h"
+
+#include <unordered_map>
+#include <vector>
+#include <memory>
+#include <string>
+#include <mutex>
+
+namespace Project::Entities {
+  class EntitiesManager {
+    public:
+      explicit EntitiesManager(LogsManager& logsManager);
+      ~EntitiesManager();
+
+      void addEntity(const std::string& id, std::shared_ptr<Entity> entity);
+      void removeEntity(const std::string& id);
+
+      void unloadSceneEntities();
+      void optimizeEntities();
+
+      void update(float deltaTime);
+      void render();
+
+    private:
+      LogsManager& logsManager;
+
+      std::unordered_map<std::string, std::shared_ptr<Entity>> activeEntities;
+      std::unordered_map<std::string, std::shared_ptr<Entity>> cachedEntities;
+
+      std::mutex entitiesMutex;
+  };
+}
+
+#endif

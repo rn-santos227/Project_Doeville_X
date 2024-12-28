bee80a218e6b6616e8c225f47e913020a372e565
rn-santos227 <rn_santos227@yahoo.com>
2024-12-28 18:45:59 +0800

    updated render function in entities manager

--- src/entities/EntitiesManager.cpp
+++ src/entities/EntitiesManager.cpp
@@ -39,7 +39,7 @@ namespace Project::Entities {
   void EntitiesManager::render() {
     std::lock_guard<std::mutex> lock(entitiesMutex);
     for (const auto& [id, entity] : activeEntities) {
-
+      entity->render();
     }
   }
 }

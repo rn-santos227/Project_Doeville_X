a0c2d45bc65a0dd7093c83fc406cd0084f60a81f
rn-santos227 <rn_santos227@yahoo.com>
2024-12-28 21:42:37 +0800

    updated render function in game state source file

--- src/states/GameState.cpp
+++ src/states/GameState.cpp
@@ -65,6 +65,10 @@ namespace Project::States {
   }
 
   void GameState::render() {
+    if (entitiesManager) {
+      
+    }
+
     lua_getglobal(luaState, "render");
     if (lua_isfunction(luaState, -1)) {
       int result = lua_pcall(luaState, 0, 0, 0);

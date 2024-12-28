d15c530c77214d8448f178665f8f7e88c08e83c4
rn-santos227 <rn_santos227@yahoo.com>
2024-12-29 01:44:58 +0800

    added decontructor in entity factory source file

--- src/factories/entity/EntityFactory.cpp
+++ src/factories/entity/EntityFactory.cpp
@@ -3,4 +3,8 @@
 namespace Project::Factories {
   EntityFactory::EntityFactory(EntitiesManager& entitiesManager, LogsManager& logsManager)
   : entitiesManager(entitiesManager), logsManager(logsManager) {}
+
+  EntityFactory::~EntityFactory() {
+    entitiesManager.reset();
+  }
 }

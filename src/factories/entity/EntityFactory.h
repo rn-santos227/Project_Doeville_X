805cc26eb70e2fe15c603d0d7c6974cf38f77d56
rn-santos227 <rn_santos227@yahoo.com>
2024-12-28 21:45:23 +0800

    updated entity factor header file

--- src/factories/entity/EntityFactory.h
+++ src/factories/entity/EntityFactory.h
@@ -1,4 +1,9 @@
 #ifndef ENTITY_FACTORY_H
 #define ENTITY_FACTORY_H
 
+#include "entities/Entity.h"
+#include "entities/EntitiesManager.h"
+
+#include "utilities/logs_manager/LogsManager.h"
+
 #endif

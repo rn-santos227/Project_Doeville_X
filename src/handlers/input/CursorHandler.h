d8f492dd5e550f2ee848fa7f70d247c8b1a21a78
rn-santos227 <rn_santos227@yahoo.com>
2024-12-29 19:36:22 +0800

    added reset to default cursor function in cursor handler header file

--- src/handlers/input/CursorHandler.h
+++ src/handlers/input/CursorHandler.h
@@ -19,6 +19,8 @@ namespace Project::Handlers {
       void setCursorState(CursorState state);
       CursorState getCursorState() const;
 
+      void resetToDefaultCursor();
+
     private:
       std::string resourcePath;
   };

b0de3630392d9fe233d0414c1e89604913beec6c
rn-santos227 <rn_santos227@yahoo.com>
2024-12-30 02:10:43 +0800

    added constructor in cursor handler source file

--- src/handlers/input/CursorHandler.cpp
+++ src/handlers/input/CursorHandler.cpp
@@ -1,1 +1,7 @@
 #include "CursorHandler.h"
+
+namespace Project::Handlers {
+  CursorHandler::CursorHandler(LogsManager& logsManager, const std::string& resourcePath) : logsManager(logsManager), resourcePath(resourcePath) {
+
+  }
+}

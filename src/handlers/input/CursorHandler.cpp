537233788db5617f53350f9c154b23f33556f811
rn-santos227 <rn_santos227@yahoo.com>
2024-12-30 02:11:15 +0800

    updated constructor in cursor handler source file

--- src/handlers/input/CursorHandler.cpp
+++ src/handlers/input/CursorHandler.cpp
@@ -3,5 +3,6 @@
 namespace Project::Handlers {
   CursorHandler::CursorHandler(LogsManager& logsManager, const std::string& resourcePath) : logsManager(logsManager), resourcePath(resourcePath) {
     defaultCursor = SDL_GetDefaultCursor();
+    cursors[CursorState::DEFAULT] = defaultCursor;
   }
 }

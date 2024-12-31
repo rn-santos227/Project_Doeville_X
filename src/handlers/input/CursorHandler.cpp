f082833d79fdb03e4c4192257f4ef118740a3745
rn-santos227 <rn_santos227@yahoo.com>
2024-12-31 12:50:02 +0800

    updated cleanup function in cursor handler source file

--- src/handlers/input/CursorHandler.cpp
+++ src/handlers/input/CursorHandler.cpp
@@ -53,7 +53,7 @@ namespace Project::Handlers {
   void CursorHandler::cleanup() {
     for (auto& pair : cursors) {
       if (pair.second && pair.second != defaultCursor) {
-
+        SDL_FreeCursor(pair.second);
       }
     }
     cursors.clear();

ed1fbf2e2be4e9db0c05576321124e718fbdcbe0
rn-santos227 <rn_santos227@yahoo.com>
2025-01-01 13:43:30 +0800

    updated create entity from lua function in enitity factory source file

--- src/factories/entity/EntityFactory.cpp
+++ src/factories/entity/EntityFactory.cpp
@@ -23,6 +23,7 @@ namespace Project::Factories {
     if (!lua_isinteger(L, -1)) {
       logsManager.logError("Lua script is missing a valid 'entityID': " + scriptPath);
       lua_close(L);
+      return false;
     }
     
     EntityID entityID = static_cast<EntityID>(lua_tointeger(L, -1));

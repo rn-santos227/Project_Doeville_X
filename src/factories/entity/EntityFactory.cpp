d86bab2fad38a0514fd1b8f414cd6f2217b0a5b2
rn-santos227 <rn_santos227@yahoo.com>
2025-01-01 13:38:03 +0800

    updated create entity from lua function in enitity factory source file

--- src/factories/entity/EntityFactory.cpp
+++ src/factories/entity/EntityFactory.cpp
@@ -17,6 +17,8 @@ namespace Project::Factories {
       lua_close(L);
       return false;
     }
+
+    lua_getglobal(L, "entityID");
     
     EntityID entityID = static_cast<EntityID>(lua_tointeger(L, -1));
     lua_pop(L, 1);

a70d13e309611b956197c95d3c2b16557cada51a
rn-santos227 <rn_santos227@yahoo.com>
2025-01-11 20:17:19 +0800

    added cmake builder class in python builder file and extends it to ibuilder

--- dependency/buiilder.py
+++ dependency/buiilder.py
@@ -3,3 +3,7 @@ import subprocess
 class IBuilder:
   def build(self, source_dir: str, output_dir: str):
     raise NotImplementedError("build method must be implemented")
+  
+class CMakeBuilder(IBuilder):
+  def build(self, source_dir: str, output_dir: str):
+    print(f"Building {source_dir} into {output_dir}...")

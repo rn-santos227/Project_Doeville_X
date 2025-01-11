c1e278c9747d85a68c8c62cde729bdfda4b09c01
rn-santos227 <rn_santos227@yahoo.com>
2025-01-11 20:10:52 +0800

    added targz extractor class in extractor python file and extend it to iextractor

--- dependency/extractor.py
+++ dependency/extractor.py
@@ -3,3 +3,7 @@ import subprocess
 class IExtractor:
   def extract(self, archive: str, destination: str):
     raise NotImplementedError("extract method must be implemented")
+  
+class TarGzExtractor(IExtractor):
+  def extract(self, archive: str, destination: str):
+    print(f"Extracting {archive} to {destination}...")

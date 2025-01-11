861a6de7abe565bb0665f0d46e4bae35446b3799
rn-santos227 <rn_santos227@yahoo.com>
2025-01-11 20:11:46 +0800

    updated targx extractor class in extractor python file

--- dependency/extractor.py
+++ dependency/extractor.py
@@ -8,3 +8,5 @@ class TarGzExtractor(IExtractor):
   def extract(self, archive: str, destination: str):
     print(f"Extracting {archive} to {destination}...")
     command = f"tar -xzf {archive} -C {destination}"
+    if subprocess.call(command, shell=True) != 0:
+      raise RuntimeError(f"Failed to extract {archive}")

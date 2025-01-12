cfdd7960084ab1740ffa51dcd1a08037b7c671e2
rn-santos227 <rn_santos227@yahoo.com>
2025-01-12 19:18:33 +0800

    updated python extractor file in dependency

--- dependency/extractor.py
+++ dependency/extractor.py
@@ -2,12 +2,8 @@ import tarfile
 import zipfile
 import os
 import subprocess
-
-class IExtractor:
-  def extract(self, archive: str, destination: str):
-    raise NotImplementedError("extract method must be implemented")
   
-class TarGzExtractor(IExtractor):
+class TarGzExtractor:
   def extract(self, archive: str, destination: str):
     print(f"Extracting {archive} to {destination}...")
     command = f"tar -xzf {archive} -C {destination}"

a8d5bab71aef6ccc23ea51fd7cfd45cb53a676c8
rn-santos227 <rn_santos227@yahoo.com>
2025-01-12 19:19:41 +0800

    updated python extractor file in dependency

--- dependency/extractor.py
+++ dependency/extractor.py
@@ -4,5 +4,9 @@ import os
 import subprocess
   
 class TarGzExtractor:
-  def extract(self, archive: str, destination: str):
-    print(f"Extracting {archive} to {destination}...")
+  def extract(self, archive_path: str, dest_folder: str):
+    print(f"Extracting {archive_path} to {dest_folder}...")
+
+    if archive_path.endswith(".tar.gz") or archive_path.endswith(".tgz"):
+      with tarfile.open(archive_path, "r:gz") as tar:
+        tar.extractall(dest_folder)

9e300fb21ffedc450b6ebd48f75669bb05e24faf
rn-santos227 <rn_santos227@yahoo.com>
2025-01-12 19:27:10 +0800

    added archive exists function in python utils file

--- dependency/utils.py
+++ dependency/utils.py
@@ -3,3 +3,6 @@ import os
 def ensure_folder_exists(folder_path):
   if not os.path.exists(folder_path):
     os.makedirs(folder_path)
+
+def archive_exists(file_path):
+  return os.path.exists(file_path)

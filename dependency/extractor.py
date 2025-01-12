c8f0a787cef4e70a7ae59bc9595041cfe4885c06
rn-santos227 <rn_santos227@yahoo.com>
2025-01-12 19:18:56 +0800

    updated python extractor file in dependency

--- dependency/extractor.py
+++ dependency/extractor.py
@@ -6,6 +6,3 @@ import subprocess
 class TarGzExtractor:
   def extract(self, archive: str, destination: str):
     print(f"Extracting {archive} to {destination}...")
-    command = f"tar -xzf {archive} -C {destination}"
-    if subprocess.call(command, shell=True) != 0:
-      raise RuntimeError(f"Failed to extract {archive}")

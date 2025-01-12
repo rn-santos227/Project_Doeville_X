05b47ca8c17e2f990318979d557930f3ead45099
rn-santos227 <rn_santos227@yahoo.com>
2025-01-12 19:37:35 +0800

    added dependency maanger class in python manager file

--- dependency/manager.py
+++ dependency/manager.py
@@ -1,3 +1,8 @@
 from dependency.downloader import HTTPDownloader
 from dependency.extractor import TarGzExtractor
 from dependency.utils import ensure_folder_exists, archive_exists
+
+class DependencyManager:
+  def __init__(self, downloader, extractor):
+    self.downloader = downloader
+    self.extractor = extractor

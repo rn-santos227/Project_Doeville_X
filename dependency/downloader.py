f7b0e07dca3ca2053aef4d2719eac1da7edd2f8e
rn-santos227 <rn_santos227@yahoo.com>
2025-01-18 15:52:32 +0800

    updated download function in python downloader file

--- dependency/downloader.py
+++ dependency/downloader.py
@@ -4,6 +4,8 @@ import urllib.request
 class HTTPDownloader:
   def download(self, url: str, destination: str):
     print(f"Downloading {url} to {destination}...")
+
+    os.makedirs(os.path.dirname(destination), exist_ok=True)
     
     with urllib.request.urlopen(url) as response, open(destination, "wb") as out_file:
       out_file.write(response.read())

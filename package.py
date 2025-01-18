7a57484c9ce4c31f1e001589c6054d9e5324c6ce
rn-santos227 <rn_santos227@yahoo.com>
2025-01-18 16:37:38 +0800

    updated load packages function in python package file

--- package.py
+++ package.py
@@ -7,3 +7,6 @@ def load_packages(package_file):
   
   if not os.path.exists(package_file):
     raise FileNotFoundError(f"Package file not found: {package_file}")
+  
+  with open(package_file, "r") as file:
+    return json.load(file)

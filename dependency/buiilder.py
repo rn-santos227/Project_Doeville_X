639f9005b5628acb195b0fd49d7b78c148ebab43
rn-santos227 <rn_santos227@yahoo.com>
2025-01-18 16:07:31 +0800

    added init function in python builder file

--- dependency/buiilder.py
+++ dependency/buiilder.py
@@ -1,9 +1,5 @@
 import subprocess
 
-class IBuilder:
-  def build(self, source_dir: str, output_dir: str):
-    raise NotImplementedError("build method must be implemented")
-  
-class CMakeBuilder(IBuilder):
-  def build(self, source_dir: str, output_dir: str):
-    print(f"Building {source_dir} into {output_dir}...")
+class Builder:
+  def __init__(self, lib_folder):
+    self.lib_folder = lib_folder

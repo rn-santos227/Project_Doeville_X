dec6d352deb81ad0aaa952d4264390bab6f61eb1
rn-santos227 <rn_santos227@yahoo.com>
2025-01-19 19:32:10 +0800

    updated python downloader file

--- dependency/downloader.py
+++ dependency/downloader.py
@@ -1,13 +1,10 @@
 import os
-import urllib.request
+import subprocess
 
 class HTTPDownloader:
   def download(self, url: str, destination: str):
     print(f"Downloading {url} to {destination}...")
 
     os.makedirs(os.path.dirname(destination), exist_ok=True)
-    
-    with urllib.request.urlopen(url) as response, open(destination, "wb") as out_file:
-      out_file.write(response.read())
 
     print(f"Downloaded: {destination}")

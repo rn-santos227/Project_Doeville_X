b67756d20ebbab2f79ccba4a3933b42f2150e311
rn-santos227 <rn_santos227@yahoo.com>
2025-01-25 20:01:20 +0800

    removed load packages funciton in python package file

--- package.py
+++ package.py
@@ -6,14 +6,6 @@ from dependency.extractor import TarGzExtractor
 from dependency.manager import DependencyManager
 from dependency.utils import ensure_folder_exists
 
-def load_packages(package_file):
-  
-  if not os.path.exists(package_file):
-    raise FileNotFoundError(f"Package file not found: {package_file}")
-  
-  with open(package_file, "r") as file:
-    return json.load(file)
-
 def main():
   root_dir = os.path.abspath(os.path.dirname(__file__))
   lib_dir = "lib"

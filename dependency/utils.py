f015e641e4c6d70f220e3f2d87ae0ab00b39ce7f
rn-santos227 <rn_santos227@yahoo.com>
2025-01-25 20:00:47 +0800

    updated load packages function in python utils file

--- dependency/utils.py
+++ dependency/utils.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 def ensure_folder_exists(folder_path):
   if not os.path.exists(folder_path):
@@ -10,3 +11,6 @@ def archive_exists(file_path):
 def load_packages(package_file):
   if not os.path.exists(package_file):
     raise FileNotFoundError(f"Package file not found: {package_file}")
+  
+  with open(package_file, "r") as file:
+    return json.load(file)

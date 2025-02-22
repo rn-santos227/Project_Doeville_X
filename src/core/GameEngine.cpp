d41044b31fc88beaebf56daf834596a133e8148c
rn-santos227 <rn_santos227@yahoo.com>
2025-02-22 18:10:01 +0800

    updated constructor in game engine source file

--- src/core/GameEngine.cpp
+++ src/core/GameEngine.cpp
@@ -15,7 +15,7 @@ namespace Project::Core {
     
     logsManager->logMessage("GameEngine constructor called.");
     screenHandler = std::make_unique<ScreenHandler>(
-      *gameStateManager, 
+      *gameStateManager, *cursorHandler, 
     );
   }
 

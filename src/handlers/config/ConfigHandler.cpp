04c9a9c1c963a1b41df427b33e37ed6c34fe4f26
rn-santos227 <rn_santos227@yahoo.com>
2025-03-02 17:56:12 +0800

    updated load config funciton in config handler source file

--- src/handlers/config/ConfigHandler.cpp
+++ src/handlers/config/ConfigHandler.cpp
@@ -11,5 +11,8 @@ namespace Project::Handlers {
 
   bool ConfigHandler::loadConfig(const std::string& filePath) {
     std::ifstream file(filePath);
+    if (!file.is_open()) {
+      return false;
+    }
   }
 }

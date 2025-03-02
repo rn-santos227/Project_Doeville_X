cd2752978bc80fc35ae5591a43f93cdfae804ead
rn-santos227 <rn_santos227@yahoo.com>
2025-03-02 17:50:25 +0800

    added config data variable in config handler header file

--- src/handlers/config/ConfigHandler.h
+++ src/handlers/config/ConfigHandler.h
@@ -19,6 +19,8 @@ namespace Project::Handlers {
   private:
     ConfigHandler() = default;
 
+    std::unordered_map<std::string, std::unordered_map<std::string, std::string>> configData;
+
   };
 }
 

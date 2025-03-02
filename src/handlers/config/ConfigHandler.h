b198f63053fd175c22b0703601bd489d764df4d9
rn-santos227 <rn_santos227@yahoo.com>
2025-03-02 17:49:22 +0800

    added get value function in config handler header file

--- src/handlers/config/ConfigHandler.h
+++ src/handlers/config/ConfigHandler.h
@@ -11,6 +11,7 @@ namespace Project::Handlers {
 
     bool loadConfig(const std::string& filePath);
 
+    std::string getValue(const std::string& section, const std::string& key, const std::string& defaultValue = "") const;
     float getFloatValue(const std::string& section, const std::string& key, float defaultValue = 0.0f) const;
     int getIntValue(const std::string& section, const std::string& key, int defaultValue = 0) const;
     bool getBoolValue(const std::string& section, const std::string& key, bool defaultValue = false) const;

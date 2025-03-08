cbac817f1bf889acb2983418445e48f9ff292989
rn-santos227 <rn_santos227@yahoo.com>
2025-03-08 19:33:29 +0800

    updated load config funciton in config handler source file

--- src/handlers/config/ConfigHandler.cpp
+++ src/handlers/config/ConfigHandler.cpp
@@ -31,6 +31,7 @@ namespace Project::Handlers {
         if (delimiterPos != std::string::npos) {
           std::string key = line.substr(0, delimiterPos);
           std::string value = line.substr(delimiterPos + 1);
+          trim(key);
         }
       }
     }

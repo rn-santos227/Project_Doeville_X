7d423591117ae48bb7be5b6cc9e3f263ec421afb
rn-santos227 <rn_santos227@yahoo.com>
2025-03-08 19:34:52 +0800

    updated get value function in config handler source file

--- src/handlers/config/ConfigHandler.cpp
+++ src/handlers/config/ConfigHandler.cpp
@@ -46,7 +46,7 @@ namespace Project::Handlers {
   std::string ConfigHandler::getValue(const std::string& section, const std::string& key, const std::string& defaultValue) const {
     auto secIt = configData.find(section);
     if (secIt != configData.end()) {
-
+      auto keyIt = secIt->second.find(key);
     }
   }
 

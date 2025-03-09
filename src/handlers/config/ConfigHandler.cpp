2b1a68567adef4a44c7d4d7cfccd15a0fb0f8748
rn-santos227 <rn_santos227@yahoo.com>
2025-03-09 21:01:06 +0800

    updated long config function in config handler source file

--- src/handlers/config/ConfigHandler.cpp
+++ src/handlers/config/ConfigHandler.cpp
@@ -9,6 +9,7 @@ namespace Project::Handlers {
   bool ConfigHandler::loadConfig(const std::string& filePath) {
     std::ifstream file(filePath);
     if (!file.is_open()) {
+      logsManager.logError("Failed to open config file: " + filePath);
       return false;
     }
 

9980da7945b21d12b9c21df584e337f4a624370d
rn-santos227 <rn_santos227@yahoo.com>
2025-03-16 14:13:11 +0800

    added animation frame struct in  animation header file

--- src/handlers/animation/Animation.h
+++ src/handlers/animation/Animation.h
@@ -1,4 +1,17 @@
 #ifndef ANIMATION_H
 #define ANIMATION_H
 
+#include "PlaybackMode.h"
+
+#include <vector>
+#include <string>
+#include <SDL.h>
+
+namespace Project::Handlers {
+  struct AnimationFrame {
+    SDL_Texture* texture;
+    Uint32 duration;
+  };
+}
+
 #endif

3629730c99b6d67752146bc2710b035d94130a19
rn-santos227 <rn_santos227@yahoo.com>
2025-03-22 16:15:46 +0800

    updated add frame function in animation source file

--- src/handlers/animation/Animation.cpp
+++ src/handlers/animation/Animation.cpp
@@ -5,7 +5,7 @@ namespace Project::Handlers {
   Animation::Animation(SDL_Renderer* renderer) : renderer(renderer) {}
 
   bool Animation::addFrame(const std::string& imagePath, Uint32 duration) {
-
+    SDL_Surface* surface = IMG_Load(imagePath.c_str());
   }
 
   void Animation::freeFrames() {

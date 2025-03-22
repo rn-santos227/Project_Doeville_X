4622fe01d360e4c5f80a8a6e1b098301f25939f7
rn-santos227 <rn_santos227@yahoo.com>
2025-03-22 16:15:26 +0800

    added add frame function in animation source file

--- src/handlers/animation/Animation.cpp
+++ src/handlers/animation/Animation.cpp
@@ -4,6 +4,10 @@
 namespace Project::Handlers {
   Animation::Animation(SDL_Renderer* renderer) : renderer(renderer) {}
 
+  bool Animation::addFrame(const std::string& imagePath, Uint32 duration) {
+
+  }
+
   void Animation::freeFrames() {
     for (auto& frame : frames) {
       SDL_DestroyTexture(frame.texture);

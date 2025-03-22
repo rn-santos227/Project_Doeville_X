d27f008c15d21e492671dd6b6a419b9520f052c5
rn-santos227 <rn_santos227@yahoo.com>
2025-03-22 16:27:51 +0800

    updated add frame function in animation source file

--- src/handlers/animation/Animation.cpp
+++ src/handlers/animation/Animation.cpp
@@ -7,13 +7,18 @@ namespace Project::Handlers {
   bool Animation::addFrame(const std::string& imagePath, Uint32 duration) {
     SDL_Surface* surface = IMG_Load(imagePath.c_str());
 
-    if (logsManager.checkAndLogError(!surface, "Failed to load texture: " + imagePath + " - " + IMG_GetError())) {
+    if (logsManager.checkAndLogError(!surface, "Failed to load image: " + imagePath + " - " + IMG_GetError())) {
       logsManager.flushLogs();
       return false;
     }
 
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
+
+    if (logsManager.checkAndLogError(!texture, "Failed to load texture: " + imagePath + " - " + IMG_GetError())) {
+      logsManager.flushLogs();
+      return false;
+    }
   }
 
   void Animation::freeFrames() {

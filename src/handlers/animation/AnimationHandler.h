288bf0948be35e264e289706c85bd00f72198bde
rn-santos227 <rn_santos227@yahoo.com>
2025-04-01 18:12:23 +0800

    imported packages and classes in animation handler header file

--- src/handlers/animation/AnimationHandler.h
+++ src/handlers/animation/AnimationHandler.h
@@ -1,4 +1,14 @@
 #ifndef ANIMATION_HANDLER_H
 #define ANIMATION_HANDLER_H
 
+#include "Animation.h"
+
+#include <unordered_map>
+#include <memory>
+#include <string>
+
+#include "utilities/logs_manager/LogsManager.h"
+
+using namespace Project::Utilities;
+
 #endif

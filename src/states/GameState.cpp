4bff5f1e5e8ff0631e695e0d64a229e836c6d4d5
rn-santos227 <rn_santos227@yahoo.com>
2025-04-05 17:08:12 +0800

    updated lua set background color function in game state source file

--- src/states/GameState.cpp
+++ src/states/GameState.cpp
@@ -177,6 +177,6 @@ namespace Project::States {
   }
 
   int lua_setBackgroundColor(lua_State* L) {
-
+    GameState* state = static_cast<GameState*>(lua_touserdata(L, lua_upvalueindex(1)));
   }
 }

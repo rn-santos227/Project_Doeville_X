ab4731b6386e3be12f556b1b85d48862f46f9baa
rn-santos227 <rn_santos227@yahoo.com>
2025-04-05 17:04:55 +0800

    updated set background color function in game state source file

--- src/states/GameState.cpp
+++ src/states/GameState.cpp
@@ -121,6 +121,7 @@ namespace Project::States {
   void GameState::setBackgroundColor(Uint8 r, Uint8 g, Uint8 b) {
     clearBackground();
     backgroundColor = {r, g, b, 255};
+    useImageBackground = false;
   }
 
   void GameState::clearBackground() {

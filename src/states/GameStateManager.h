cbe895616362854e7804d89ebbf48ccb259b69f6
rn-santos227 <rn_santos227@yahoo.com>
2025-04-06 18:52:40 +0800

    updated render function in game state header file

--- src/states/GameStateManager.h
+++ src/states/GameStateManager.h
@@ -25,7 +25,7 @@ namespace Project::States {
     void popState();
     
     void update(float deltaTime);
-    void render();
+    void render(SDL_Renderer* renderer);
 
     void enableStates(const std::vector<std::string>& names);
     void disableStates(const std::vector<std::string>& names);

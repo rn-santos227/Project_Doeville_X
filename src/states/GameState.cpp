625f73ea53a4ce3b94ad49f29e1445a91496edc6
rn-santos227 <rn_santos227@yahoo.com>
2025-04-09 18:12:50 +0800

    updated initialize function in game state source file

--- src/states/GameState.cpp
+++ src/states/GameState.cpp
@@ -33,15 +33,7 @@ namespace Project::States {
   }
 
   void GameState::initialize() {
-    lua_getglobal(luaState, "initialize");
-    if (lua_isfunction(luaState, -1)) {
-      int result = lua_pcall(luaState, 0, 0, 0);
-      if (result != LUA_OK) {
-        handleLuaError("Error calling Lua function 'initialize': " + std::string(lua_tostring(luaState, -1)));
-      } 
-    } else {
-      lua_pop(luaState, 1);
-    }
+    
   }
 
   void GameState::onEnter() {

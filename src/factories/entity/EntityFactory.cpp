968b0dd4f35879c6bafd274db75aa8c765b1dc3c
rn-santos227 <rn_santos227@yahoo.com>
2025-04-17 12:20:30 +0800

    updated decconstructor in entity factory source file

--- src/factories/entity/EntityFactory.cpp
+++ src/factories/entity/EntityFactory.cpp
@@ -1,5 +1,9 @@
 #include "EntityFactory.h"
 
 namespace Project::Factories {
+  EntityFactory::EntityFactory(LogsManager& logsManager) : logsManager(logsManager) {}
 
+  EntityFactory::~EntityFactory() {
+
+  }
 }

701f02574e2cc66252338e43f76647ded1ff2c8e
rn-santos227 <rn_santos227@yahoo.com>
2025-04-18 00:56:06 +0800

    updated constructor in entity source file

--- src/entities/Entity.cpp
+++ src/entities/Entity.cpp
@@ -4,6 +4,9 @@ namespace Project::Entities {
   Entity::Entity(EntityID entityID, LogsManager& logsManager)
   : entityID(std::move(entityID)), logsManager(logsManager) {
     luaState = luaL_newstate();
+    if (luaState) {
+      luaL_openlibs(luaState);
+    }
   }
 
   void Entity::handleLuaError(const std::string& errorMessage) {

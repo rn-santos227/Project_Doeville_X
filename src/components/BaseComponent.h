30bb1538ff9e25ad63f2964edea3a55e364a5cf8
rn-santos227 <rn_santos227@yahoo.com>
2025-04-18 11:20:47 +0800

    updated base component header file

--- src/components/BaseComponent.h
+++ src/components/BaseComponent.h
@@ -5,9 +5,13 @@ namespace Project::Components {
   class BaseComponent {
   public:
     virtual ~BaseComponent() = default;
+    
+    virtual void initialize() {}
 
     virtual void update(float deltaTime) = 0;
     virtual void render() = 0;
+
+    virtual void shutdown() {}
   };
 }
 

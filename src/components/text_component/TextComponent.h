5e458395a84c7009f5370f6eda1407e439c97e2c
rn-santos227 <rn_santos227@yahoo.com>
2025-04-22 19:43:05 +0800

    added deconstructor in text component header file

--- src/components/text_component/TextComponent.h
+++ src/components/text_component/TextComponent.h
@@ -10,6 +10,7 @@ namespace Project::Components {
   class TextComponent : public BaseComponent {
   public:
     TextComponent(SDL_Renderer* renderer, const std::string& text, const std::string& fontPath, int fontSize, SDL_Color color);
+    ~TextComponent();
   };
 }
 

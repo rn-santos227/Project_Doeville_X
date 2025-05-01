23c627042e7a5b140a4115e2237ae589d890e14f
rn-santos227 <rn_santos227@yahoo.com>
2025-05-01 12:53:05 +0800

    added constructor in graphics component source file

--- src/components/grahpics_component/GraphicsComponent.cpp
+++ src/components/grahpics_component/GraphicsComponent.cpp
@@ -2,5 +2,8 @@
 #include <SDL_image.h>
 
 namespace Project::Components {
+  GraphicsComponent::GraphicsComponent(SDL_Renderer* renderer, LogsManager& logsManager)
+  : BaseComponent(logsManager), renderer(renderer), logsManager(logsManager) {
 
+  }
 }

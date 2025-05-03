db6d9512a5eb54530f9b4fcaa376d3c314ba353a
rn-santos227 <rn_santos227@yahoo.com>
2025-05-03 09:44:49 +0800

    added constructor in text component source file

--- src/components/text_component/TextComponent.cpp
+++ src/components/text_component/TextComponent.cpp
@@ -1,5 +1,8 @@
 #include "TextComponent.h"
 
 namespace Project::Components {
-
+  TextComponent::TextComponent(SDL_Renderer* renderer, TTF_Font* font,  SDL_Color color, const std::string& text, const std::string& fontPath, int fontSize, LogsManager& logsManager)
+  : BaseComponent(logsManager), renderer(renderer), font(font), color(color), currentText(text), fontPath(fontPath), animationHandler(renderer, logsManager) {
+    
+  }
 }

b8b5dc81a38ba6659f6e9237c09a2d87c99c67ff
rn-santos227 <rn_santos227@yahoo.com>
2025-05-03 09:45:17 +0800

    added create texture function in text component source file

--- src/components/text_component/TextComponent.cpp
+++ src/components/text_component/TextComponent.cpp
@@ -3,6 +3,10 @@
 namespace Project::Components {
   TextComponent::TextComponent(SDL_Renderer* renderer, TTF_Font* font,  SDL_Color color, const std::string& text, const std::string& fontPath, int fontSize, LogsManager& logsManager)
   : BaseComponent(logsManager), renderer(renderer), font(font), color(color), currentText(text), fontPath(fontPath), animationHandler(renderer, logsManager) {
-    
+
+  }
+
+  void TextComponent::createTexture() {
+
   }
 }

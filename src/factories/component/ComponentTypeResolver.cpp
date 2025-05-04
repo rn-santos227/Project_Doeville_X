8e12e26d6e17ca2ff7abde2d9e6aedd49ec82b8f
rn-santos227 <rn_santos227@yahoo.com>
2025-05-04 13:01:19 +0800

    updated resolve function in component type  resolver source file

--- src/factories/component/ComponentTypeResolver.cpp
+++ src/factories/component/ComponentTypeResolver.cpp
@@ -3,5 +3,6 @@
 ComponentType ComponentTypeResolver::resolve(const std::string& name) {
   static const std::unordered_map<std::string, ComponentType> map = {
     {"GraphicsComponent", ComponentType::GRAPHICS},
+    {"TextComponent", ComponentType::TEXT},
   };
 }

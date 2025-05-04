a340365dd176515287ce8661f9b1f665da11dfae
rn-santos227 <rn_santos227@yahoo.com>
2025-05-04 13:53:40 +0800

    added constructor in componest factory source file

--- src/factories/component/ComponentFactory.cpp
+++ src/factories/component/ComponentFactory.cpp
@@ -1,5 +1,6 @@
 #include "ComponentFactory.h"
 
 namespace Project::Factories {
-
+  ComponentFactory::ComponentFactory(SDL_Renderer* renderer, LogsManager& logsManager)
+  : renderer(renderer), logsManager(logsManager) {}
 }

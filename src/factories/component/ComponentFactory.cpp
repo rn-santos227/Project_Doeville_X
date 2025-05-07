a4eb62485826645e142f44838aa7a809113b9b5a
rn-santos227 <rn_santos227@yahoo.com>
2025-05-07 19:54:11 +0800

    updated create function in component factory source file

--- src/factories/component/ComponentFactory.cpp
+++ src/factories/component/ComponentFactory.cpp
@@ -8,40 +8,7 @@ namespace Project::Factories {
     ComponentType type = ComponentTypeResolver::resolve(componentName);
     
     switch (type) {
-      case ComponentType::GRAPHICS: {
-        auto graphicsComponent = std::make_unique<GraphicsComponent>(renderer, logsManager);
-        graphicsComponent->onAttach();
-        return graphicsComponent;
-      }
-
-      case ComponentType::TEXT: {
-        lua_getglobal(lua, "text");
-        std::string text = lua_isstring(lua, -1) ? lua_tostring(lua, -1) : "Default Text";
-        lua_pop(lua, 1);
-
-        lua_getglobal(lua, "fontPath");
-        std::string fontPath = lua_isstring(lua, -1) ? lua_tostring(lua, -1) : "assets/fonts/default.ttf";
-        lua_pop(lua, 1);
-
-        lua_getglobal(lua, "fontSize");
-        int fontSize = lua_isinteger(lua, -1) ? lua_tointeger(lua, -1) : 16;
-        lua_pop(lua, 1);
-
-        SDL_Color color = getLuaSDLColor(lua);
-
-        TTF_Font* font = TTF_OpenFont(fontPath.c_str(), fontSize);
-        if (logsManager.checkAndLogError(font == nullptr, "Failed to load font: " + fontPath)) {
-          logsManager.flushLogs();
-          return nullptr;
-        }
-
-        return std::make_unique<TextComponent>(renderer, font, color, text, fontPath, fontSize, logsManager);
-      }
-
-      default: {
-        logsManager.logError("Unknown or unsupported component type: " + componentName);
-        return nullptr;
-      }
+      
     }
   }
 

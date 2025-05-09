6a82c8745d495cd7cb0028b658fa6ec31f54fe35
rn-santos227 <rn_santos227@yahoo.com>
2025-05-09 19:35:24 +0800

    updated create text component function in components factory source file

--- src/factories/component/ComponentsFactory.cpp
+++ src/factories/component/ComponentsFactory.cpp
@@ -34,6 +34,9 @@ namespace Project::Factories {
   std::unique_ptr<BaseComponent> ComponentsFactory::createTextComponent(lua_State* lua) {
     SDL_Color color = getLuaSDLColor(lua);
     std::string text = getLuaGlobalString(lua, "text", "Default Text");
+
+    std::string defaultFontPath = configHandler.getValue("Paths", "default_font", "resources/fonts/system.ttf");
+    
     std::string fontPath = getLuaGlobalString(lua, "fontPath", "resources/fonts/system.ttf");
     int fontSize = getLuaGlobalInt(lua, "fontSize", 16);
 

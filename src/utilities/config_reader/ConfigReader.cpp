3669ffbff8ef3687fa22b1f644efaa387f605262
rn-santos227 <rn_santos227@yahoo.com>
2025-05-09 20:50:52 +0800

    added get double value function in config reader header file

--- src/utilities/config_reader/ConfigReader.cpp
+++ src/utilities/config_reader/ConfigReader.cpp
@@ -16,6 +16,7 @@ namespace Project::Utilities {
     bool loadConfig(const std::string& filePath);
 
     std::string getValue(const std::string& section, const std::string& key, const std::string& defaultValue = "") const;
+    double getDoubleValue(const std::string& section, const std::string& key, double defaultValue = 0.0) const;
     float getFloatValue(const std::string& section, const std::string& key, float defaultValue = 0.0f) const;
     int getIntValue(const std::string& section, const std::string& key, int defaultValue = 0) const;
     bool getBoolValue(const std::string& section, const std::string& key, bool defaultValue = false) const;

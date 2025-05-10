690e29662f59b8665f5ad9a4fc048a4760f5bd51
rn-santos227 <rn_santos227@yahoo.com>
2025-05-10 11:53:10 +0800

    updated load config function in config reader source file

--- src/utilities/config_reader/ConfigReader.cpp
+++ src/utilities/config_reader/ConfigReader.cpp
@@ -23,6 +23,8 @@ namespace Project::Utilities {
       if (line[0] == '[' && line.back() == ']') {
         section = line.substr(1, line.size() - 2);
         trim(section);
+      } else {
+        size_t delimiterPos = line.find('=');
       }
     }
   }

2bc8066406e7439cc74b11c8fac9ea8867c20830
rn-santos227 <rn_santos227@yahoo.com>
2025-05-14 21:10:55 +0800

    updated create entity from lua function in entities factory source file

--- src/factories/entity/EntitiesFactory.cpp
+++ src/factories/entity/EntitiesFactory.cpp
@@ -12,7 +12,7 @@ namespace Project::Factories {
     std::unique_ptr<Entity> entity = loadEntityTemplateFromLua(scriptPath);
     if(logsManager.checkAndLogError(!entity, "Failed to load entity template from Lua: " + scriptPath)) {
       logsManager.flushLogs();
-      return false;
+      return nullptr;
     }
 
     std::string name = entity->getEntityName();

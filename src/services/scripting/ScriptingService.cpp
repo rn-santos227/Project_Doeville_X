2f217309a07c641ad11c8b6bf0f48723881a66f6
rn-santos227 <rn_santos227@yahoo.com>
2025-05-14 19:59:40 +0800

    updated load scripts from folder function in scripting service source file

--- src/services/scripting/ScriptingService.cpp
+++ src/services/scripting/ScriptingService.cpp
@@ -38,9 +38,7 @@ namespace Project::Services {
       const std::string scriptPath = entry.path().string();
       const std::string scriptName = entry.path().filename().string();
 
-      if (scriptName.find(".lua") == std::string::npos) {
-        continue;
-      }
+      if (scriptName.find(".lua") == std::string::npos) continue;
 
       ScriptCategory category = determineScriptType(scriptName);
       if (logsManager.checkAndLogError(category == ScriptCategory::INVALID, "Invalid script type for file: " + scriptPath)) {

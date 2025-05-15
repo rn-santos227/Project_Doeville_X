594d074034a16d23994043149181a0ea88d7baa0
rn-santos227 <rn_santos227@yahoo.com>
2025-05-15 19:54:10 +0800

    updated constructor in lua script wrapper source file

--- src/utilities/lua_state_wrapper/LuaStateWrapper.cpp
+++ src/utilities/lua_state_wrapper/LuaStateWrapper.cpp
@@ -2,6 +2,8 @@
 
 namespace Project::Utilities {
   LuaStateWrapper::LuaStateWrapper(LogsManager& logsManager) : luaState(luaL_newstate()), logsManager(logsManager) {
-
+    if (luaState) {
+      luaL_openlibs(luaState);
+    }
   }
 }

72342109ae98ed7cc5ec2b2acf637b54a89cc1f2
rn-santos227 <rn_santos227@yahoo.com>
2025-05-16 19:43:30 +0800

    updated call lua function in game state source file

--- src/states/GameState.cpp
+++ src/states/GameState.cpp
@@ -23,7 +23,7 @@ namespace Project::States {
     lua_State* L = luaStateWrapper.get();
     lua_getglobal(L, functionName.c_str());
 
-    if (!lua_isfunction(luaState, -1)) {
+    if (!lua_isfunction(L, -1)) {
       handleLuaError("Lua function not found: " + functionName);
       lua_pop(luaState, 1);
       return false;

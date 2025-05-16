b01b1a1bc78bd7dcd50102e5c509d5e0275fb0f6
rn-santos227 <rn_santos227@yahoo.com>
2025-05-17 00:03:25 +0800

    updated handle input function in game state source file

--- src/states/GameState.cpp
+++ src/states/GameState.cpp
@@ -109,7 +109,7 @@ namespace Project::States {
   void GameState::handleInput() {
     lua_State* L = luaStateWrapper.get();
     lua_getglobal(L, "handleInput");
-    if (lua_isfunction(luaState, -1)) {
+    if (lua_isfunction(L, -1)) {
       int result = lua_pcall(luaState, 0, 0, 0);
       if (result != LUA_OK) {
         handleLuaError("Error calling Lua function 'handleInput': " + std::string(lua_tostring(luaState, -1)));

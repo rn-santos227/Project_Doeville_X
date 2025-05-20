98c48ce5b93a0a3c0ddbe9ad7370ec1364fae332
rn-santos227 <rn_santos227@yahoo.com>
2025-05-20 19:48:59 +0800

    updated call global function function in lua state wrapper source file

--- src/utilities/lua_state_wrapper/LuaStateWrapper.cpp
+++ src/utilities/lua_state_wrapper/LuaStateWrapper.cpp
@@ -97,6 +97,7 @@ namespace Project::Utilities {
     if (lua_pcall(luaState, nargs, nresults, 0) != LUA_OK) {
       const char* error = lua_tostring(luaState, -1);
       handleLuaError(std::string("Lua function call failed: ") + (error ? error : "Unknown error"));
+      lua_pop(luaState, 1);
     }
   }
 

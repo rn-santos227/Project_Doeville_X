54f8d95a861b657d3b70ddc952d250b4ce319075
rn-santos227 <rn_santos227@yahoo.com>
2025-05-21 19:24:08 +0800

    updated lua print redirect function in lua state wrapper source file

--- src/utilities/lua_state_wrapper/LuaStateWrapper.cpp
+++ src/utilities/lua_state_wrapper/LuaStateWrapper.cpp
@@ -132,6 +132,7 @@ namespace Project::Utilities {
 
   int LuaStateWrapper::luaPrintRedirect(lua_State* L) {
     int nargs = lua_gettop(L);
+    std::string output;
 
     for (int i = 1; i <= nargs; ++i) {
 

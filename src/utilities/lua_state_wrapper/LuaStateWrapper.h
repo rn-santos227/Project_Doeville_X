3873f0568f1e3ae65a48a98fdd004271062189c0
rn-santos227 <rn_santos227@yahoo.com>
2025-05-22 21:09:22 +0800

    added set global number function in lua state wrapper header file

--- src/utilities/lua_state_wrapper/LuaStateWrapper.h
+++ src/utilities/lua_state_wrapper/LuaStateWrapper.h
@@ -27,6 +27,7 @@ namespace Project::Utilities {
     //Global Setters
     void setGlobalString(const std::string& name, const std::string& value) const;
     void setGlobalNumber(const std::string& name, float value);
+    void setGlobalBoolean(const std::string& name, bool value);
 
     bool isGlobalFunction(const std::string& name) const;
     bool callGlobalFunction(const std::string& name, int nargs = 0, int nresults = 0) const;

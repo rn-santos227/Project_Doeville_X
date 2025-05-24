5b000d725915983a844a707a59cc878f3d05d832
rn-santos227 <rn_santos227@yahoo.com>
2025-05-24 12:15:54 +0800

    updated get table string function in lua state wrapper source file

--- src/utilities/lua_state_wrapper/LuaStateWrapper.cpp
+++ src/utilities/lua_state_wrapper/LuaStateWrapper.cpp
@@ -151,7 +151,8 @@ namespace Project::Utilities {
 
     lua_getglobal(luaState, tableName.c_str());
     if (!lua_istable(luaState, -1)) {
-
+      lua_pop(luaState, 1);
+      return defaultValue;
     }
   }
 

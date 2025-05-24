e7175e7c501c8cb5049c07d45173f8bf17a69bed
rn-santos227 <rn_santos227@yahoo.com>
2025-05-24 12:16:24 +0800

    updated get table string function in lua state wrapper source file

--- src/utilities/lua_state_wrapper/LuaStateWrapper.cpp
+++ src/utilities/lua_state_wrapper/LuaStateWrapper.cpp
@@ -154,6 +154,8 @@ namespace Project::Utilities {
       lua_pop(luaState, 1);
       return defaultValue;
     }
+
+    lua_pushstring(luaState, key.c_str());
   }
 
   void LuaStateWrapper::setTableString(const std::string& tableName, const std::string& key, const std::string& value) {

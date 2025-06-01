a52d733833be76da421c97e4ec87354a5f55c3ca
rn-santos227 <rn_santos227@yahoo.com>
2025-06-01 15:37:46 +0800

    updated clear function in sdl manager source file

--- src/core/SDLManager.cpp
+++ src/core/SDLManager.cpp
@@ -37,6 +37,7 @@ namespace Project::Core {
   void SDLManager::clear() {
     if (renderer) {
       SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+      SDL_RenderClear(renderer);
     } else {
       logsManager.logError("Attempted to clear but renderer is null.");  
     }

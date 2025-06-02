2a088f4537da1bf2b62ed5becba550c128437b7d
rn-santos227 <rn_santos227@yahoo.com>
2025-06-02 18:42:32 +0800

    updated init function in  game engine source file

--- src/core/GameEngine.cpp
+++ src/core/GameEngine.cpp
@@ -23,6 +23,7 @@ namespace Project::Core {
     std::string title = configReader.getValue("Window", "title", "Project Doeville X");
     int screenWidth = configReader.getIntValue("Window", "width", 800);
     int screenHeight = configReader.getIntValue("Window", "height", 600);
+    bool isFullscreen = configReader.getBoolValue("Window", "fullscreen", false);
 
 
 

ecb01d83aa31443177ecc29471abf37371727c2c
rn-santos227 <rn_santos227@yahoo.com>
2025-06-03 20:47:49 +0800

    updated clone entity function in entities factory source file

--- src/factories/entity/EntitiesFactory.cpp
+++ src/factories/entity/EntitiesFactory.cpp
@@ -30,7 +30,6 @@ namespace Project::Factories {
   std::unique_ptr<Entity> EntitiesFactory::cloneEntity(const std::string& entityName) {
     auto it = entityTemplates.find(entityName);
     if(logsManager.checkAndLogError(it == entityTemplates.end(), "Entity template not found: " + entityName)) {
-      logsManager.flushLogs();
       return nullptr;
     }
 

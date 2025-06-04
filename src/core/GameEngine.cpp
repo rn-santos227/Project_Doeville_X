7323a23572c058aabb9cb6ce6e457c67bb2b6830
rn-santos227 <rn_santos227@yahoo.com>
2025-06-04 20:07:50 +0800

    updated constructor function in game engine source file

--- src/core/GameEngine.cpp
+++ src/core/GameEngine.cpp
@@ -14,7 +14,7 @@ namespace Project::Core {
     logsManager, framesCounter, configReader, sdlManager,
     *componentsFactory, *gameStateManager,
     *cursorHandler, *fontHandler, *keyHandler,
-  ))
+    *mouseHandler, *resourcesHandler))
   {}
   
   GameEngine::~GameEngine() {

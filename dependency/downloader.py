613e3280e44e19fb7d93df5f000ce6aa1b12d05e
rn-santos227 <rn_santos227@yahoo.com>
2025-06-05 18:45:12 +0800

    updated download function in dependency downloader file

--- dependency/downloader.py
+++ dependency/downloader.py
@@ -5,3 +5,9 @@ class HTTPDownloader:
   def download(self, url: str, destination: str):
     print(f"Downloading {url} to {destination}...")
     os.makedirs(os.path.dirname(destination), exist_ok=True)
+
+    try:
+      pass
+
+    except subprocess.CalledProcessError as e:
+      pass

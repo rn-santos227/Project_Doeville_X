3ad5563ad9b9d34db7e2e8ee1372edf7566d527c
rn-santos227 <rn_santos227@yahoo.com>
2025-06-05 18:44:53 +0800

    updated download function in dependency downloader file

--- dependency/downloader.py
+++ dependency/downloader.py
@@ -4,4 +4,4 @@ import subprocess
 class HTTPDownloader:
   def download(self, url: str, destination: str):
     print(f"Downloading {url} to {destination}...")
-    
+    os.makedirs(os.path.dirname(destination), exist_ok=True)

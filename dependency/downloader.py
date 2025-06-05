937062bb1b004965c39f2d60adf240887bd470b8
rn-santos227 <rn_santos227@yahoo.com>
2025-06-05 18:44:41 +0800

    updated download function in dependency downloader file

--- dependency/downloader.py
+++ dependency/downloader.py
@@ -4,15 +4,4 @@ import subprocess
 class HTTPDownloader:
   def download(self, url: str, destination: str):
     print(f"Downloading {url} to {destination}...")
-
-    os.makedirs(os.path.dirname(destination), exist_ok=True)
-
-    try:
-      print(f"Downloaded: {destination}")
-      subprocess.run(
-        ["curl", "-L", "-o", destination, url],
-        check=True
-      )
-
-    except subprocess.CalledProcessError as e:
-      print(f"Error downloading {url}: {e}")
+    

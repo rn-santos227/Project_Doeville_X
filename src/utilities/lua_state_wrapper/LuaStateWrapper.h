5a8bf96f6d218f9968e92f6f9b3286dc5a34d6a5
rn-santos227 <rn_santos227@yahoo.com>
2025-06-07 11:50:42 +0800

    updated lua state wrapper source and header files

--- src/utilities/lua_state_wrapper/LuaStateWrapper.h
+++ src/utilities/lua_state_wrapper/LuaStateWrapper.h
@@ -15,7 +15,43 @@ namespace Project::Utilities {
 
     // Generic argument-based function call
     template<typename... Args>
-    bool callGlobalFunctionWithArgs(const std::string& name, int nresults, Args... args);
+    bool LuaStateWrapper::callGlobalFunctionWithArgs(const std::string& name, int nresults, Args... args) {
+      if (!luaState) return false;
+
+      lua_getglobal(luaState, name.c_str());
+      if (!lua_isfunction(luaState, -1)) {
+        lua_pop(luaState, 1); 
+        logsManager.logError("Lua: '" + name + "' is not a function.");
+        return false;
+      }
+
+      auto push = [this](auto value) {
+        using T = std::decay_t<decltype(value)>;
+        if constexpr (std::is_same_v<T, int>) {
+          lua_pushinteger(luaState, static_cast<lua_Integer>(value));
+        } else if constexpr (std::is_same_v<T, float> || std::is_same_v<T, double>) {
+          lua_pushnumber(luaState, static_cast<lua_Number>(value));
+        } else if constexpr (std::is_same_v<T, bool>) {
+          lua_pushboolean(luaState, value);
+        } else if constexpr (std::is_same_v<T, const char*>) {
+          lua_pushstring(luaState, value);
+        } else if constexpr (std::is_same_v<T, std::string>) {
+          lua_pushstring(luaState, value.c_str());
+        } else {
+          logsManager.logError("Unsupported Lua argument type.");
+        }
+      };
+
+      (push(args), ...);
+      int argCount = sizeof...(Args);
+      int result = lua_pcall(luaState, argCount, nresults, 0);
+      if (result != LUA_OK) {
+        handleLuaError(result);
+        return false;
+      }
+
+      return true;
+    }
 
     lua_State* get() const;
     

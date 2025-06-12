6ddbf22fb90ac505f6ac8d88fe41b623f535dbe1
rn-santos227 <rn_santos227@yahoo.com>
2025-06-12 14:59:07 +0800

    updated asnyc resource loader header file

--- src/handlers/resources/AsyncResourceLoader.h
+++ src/handlers/resources/AsyncResourceLoader.h
@@ -1,3 +1,19 @@
 #ifndef ASYNC_RESOURCE_LOADER_H
+#define ASYNC_RESOURCE_LOADER_H
+
+#include <atomic>
+#include <condition_variable>
+#include <future>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <thread>
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+#include "utilities/logs_manager/LogsManager.h"
+
+using namespace Project::Utilities;
 
 #endif

f25139c0ebb01167c2ef158bf67c3ded3248de92
rn-santos227 <rn_santos227@yahoo.com>
2025-06-13 21:22:13 +0800

    updated load surface function in async resource loader source file

--- src/handlers/resources/AsyncResourceLoader.cpp
+++ src/handlers/resources/AsyncResourceLoader.cpp
@@ -9,5 +9,6 @@ namespace Project::Handlers {
   std::future<SDL_Surface*> AsyncResourceLoader::loadSurface(const std::string& imagePath) {
     Task task;
     task.path = imagePath;
+    std::future<SDL_Surface*> fut = task.promise.get_future();
   }
 }

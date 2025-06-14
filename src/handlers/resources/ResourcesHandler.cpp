95070048a516dbd2c3ed9e0b94a8ec867db436f1
rn-santos227 <rn_santos227@yahoo.com>
2025-06-14 11:00:21 +0800

    added load texture async function in resources handler source file

--- src/handlers/resources/ResourcesHandler.cpp
+++ src/handlers/resources/ResourcesHandler.cpp
@@ -64,6 +64,34 @@ namespace Project::Handlers {
     return texture;
   }
 
+  std::future<SDL_Texture*> ResourcesHandler::loadTextureAsync(SDL_Renderer* renderer, const std::string& imagePath) {
+
+  }
+
+  SDL_Texture* ResourcesHandler::cropImage(SDL_Renderer* renderer, const std::string& imagePath, SDL_Rect cropRect) {
+    SDL_Texture* fullTexture = loadTexture(renderer, imagePath);
+    if (!fullTexture) return nullptr;
+
+    int texWidth, texHeight;
+    SDL_QueryTexture(fullTexture, nullptr, nullptr, &texWidth, &texHeight);
+
+    if (cropRect.x < 0 || cropRect.y < 0 || cropRect.w <= 0 || cropRect.h <= 0 || cropRect.x + cropRect.w > texWidth || cropRect.y + cropRect.h > texHeight) {
+      logsManager.logWarning("Invalid crop dimensions: (" + 
+        std::to_string(cropRect.x) + "," + std::to_string(cropRect.y) + "," + 
+        std::to_string(cropRect.w) + "," + std::to_string(cropRect.h) + 
+        ") (Image size: " + std::to_string(texWidth) + "x" + std::to_string(texHeight) + ")");
+    
+      return nullptr;
+    }
+
+    SDL_Texture* croppedTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, cropRect.w, cropRect.h);
+    SDL_SetRenderTarget(renderer, croppedTexture);
+    SDL_RenderCopy(renderer, fullTexture, &cropRect, nullptr);
+    SDL_SetRenderTarget(renderer, nullptr);
+
+    return croppedTexture;
+  }
+
   std::vector<SDL_Texture*> ResourcesHandler::sliceImage(SDL_Renderer* renderer, const std::string& imagePath, int frameWidth, int frameHeight) {
     std::vector<SDL_Texture*> frames;
     SDL_Texture* fullTexture = loadTexture(renderer, imagePath);
@@ -90,29 +118,5 @@ namespace Project::Handlers {
       }
     }
     return frames;
-  }
-
-  SDL_Texture* ResourcesHandler::cropImage(SDL_Renderer* renderer, const std::string& imagePath, SDL_Rect cropRect) {
-    SDL_Texture* fullTexture = loadTexture(renderer, imagePath);
-    if (!fullTexture) return nullptr;
-
-    int texWidth, texHeight;
-    SDL_QueryTexture(fullTexture, nullptr, nullptr, &texWidth, &texHeight);
-
-    if (cropRect.x < 0 || cropRect.y < 0 || cropRect.w <= 0 || cropRect.h <= 0 || cropRect.x + cropRect.w > texWidth || cropRect.y + cropRect.h > texHeight) {
-      logsManager.logWarning("Invalid crop dimensions: (" + 
-        std::to_string(cropRect.x) + "," + std::to_string(cropRect.y) + "," + 
-        std::to_string(cropRect.w) + "," + std::to_string(cropRect.h) + 
-        ") (Image size: " + std::to_string(texWidth) + "x" + std::to_string(texHeight) + ")");
-    
-      return nullptr;
-    }
-
-    SDL_Texture* croppedTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, cropRect.w, cropRect.h);
-    SDL_SetRenderTarget(renderer, croppedTexture);
-    SDL_RenderCopy(renderer, fullTexture, &cropRect, nullptr);
-    SDL_SetRenderTarget(renderer, nullptr);
-
-    return croppedTexture;
   }
 }

112e32bfdc9f21d5785f993beb2dccd91b2a3f0e
rn-santos227 <rn_santos227@yahoo.com>
2025-06-14 11:06:51 +0800

    updated load texture async function in resources handler source file

--- src/handlers/resources/ResourcesHandler.cpp
+++ src/handlers/resources/ResourcesHandler.cpp
@@ -94,6 +94,7 @@ namespace Project::Handlers {
           std::lock_guard<std::mutex> lock(textureCacheMutex);
           textureCache[imagePath] = texture;
         }
+        return texture;
     });
   }
 

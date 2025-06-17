567a92f9bddbaa175f42c724d53af6e99933a84f
rn-santos227 <rn_santos227@yahoo.com>
2025-06-17 20:03:35 +0800

    updated resource cleaner header file

--- src/utilities/resource_cleaner/ResourceCleaner.h
+++ src/utilities/resource_cleaner/ResourceCleaner.h
@@ -7,7 +7,9 @@ namespace Project::Utilities {
   struct ResourceCleaner {
     template<typename AssocContainer, typename Deleter>
     static void cleanupMap(AssocContainer& container, Deleter deleter) {
+      for (auto& pair : container) {
 
+      }
     }
   };
 }

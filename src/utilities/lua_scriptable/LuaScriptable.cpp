c9da31cd4af88404261fa1dd52af6b9b6f823840
rn-santos227 <rn_santos227@yahoo.com>
2025-06-18 19:47:11 +0800

    updated constructor in lua scriptable source file

--- src/utilities/lua_scriptable/LuaScriptable.cpp
+++ src/utilities/lua_scriptable/LuaScriptable.cpp
@@ -4,7 +4,8 @@ namespace Project::Utilities {
   LuaScriptable::LuaScriptable(LogsManager& logsManager)
     : logsManager(logsManager), luaStateWrapper(logsManager) {
     if (luaStateWrapper.isValid()) {
-
+      luaL_openlibs(luaStateWrapper.get());
+      luaStateWrapper.registerFunction("print", LuaStateWrapper::luaPrintRedirect);
     } else {
       logsManager.logError("Failed to create Lua state");
     }

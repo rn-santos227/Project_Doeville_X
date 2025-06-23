ac377957375041cee2fb1277f8e999de3123315b
rn-santos227 <rn_santos227@yahoo.com>
2025-06-23 18:14:46 +0800

    update lua spawn entity function in game state source file

--- src/states/GameState.cpp
+++ src/states/GameState.cpp
@@ -149,8 +149,8 @@ namespace Project::States {
 
   int Project::States::GameState::lua_spawnEntity(lua_State* L) {
     GameState* state = static_cast<GameState*>(lua_touserdata(L, lua_upvalueindex(1)));
-     if (!state) {
-
-     }
+    if (!state) {
+      return luaL_error(L, "Invalid GameState reference in lua_spawnEntity.");
+    }
   }
 }

08da0ab668e9d3adf6a1bc4fd5b83e40917ca363
rn-santos227 <rn_santos227@yahoo.com>
2025-06-23 18:37:14 +0800

    menu state lua file in script

--- scripts/states/menu.state.lua
+++ scripts/states/menu.state.lua
@@ -5,6 +5,7 @@ sampleText = dofile("scripts/entities/sample_text.entity.lua")
 function initialize()
   print("Initializing MainMenuState")
   setBackgroundColor(34, 45, 50, 255)
+  spawnEntity("sample_text")
 end
 
 function onEnter()

f07136f0b49e01121a3d3be99e22fe252e21412a
rn-santos227 <rn_santos227@yahoo.com>
2025-06-26 22:20:52 +0800

    updated sample text entity lua file

--- scripts/entities/sample_text.entity.lua
+++ scripts/entities/sample_text.entity.lua
@@ -6,6 +6,10 @@ components = {
   TextComponent = {
     text = "Sample Text",
     fontColorR = 255,
+    fontColorG = 255,
+    fontColorB = 255,
+    fontColorA = 255,
+    fontSize = 24
   }
 }
 

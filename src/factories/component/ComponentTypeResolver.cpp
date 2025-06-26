3b073a7e8b2327fc7ef64dff014c21062162636a
rn-santos227 <rn_santos227@yahoo.com>
2025-06-26 22:59:51 +0800

    updated resolve function in component type resolver source file

--- src/factories/component/ComponentTypeResolver.cpp
+++ src/factories/component/ComponentTypeResolver.cpp
@@ -11,7 +11,10 @@ ComponentType ComponentTypeResolver::resolve(const std::string& name) {
     {"text", ComponentType::TEXT},
   };
 
-  auto it = map.find(name);
+  std::string key = name;
+  std::transform(key.begin(), key.end(), key.begin(), [](unsigned char c) { return std::tolower(c); });
+
+  auto it = map.find(key);
   if (it != map.end()) return it->second;
   return ComponentType::UNKNOWN;
 }

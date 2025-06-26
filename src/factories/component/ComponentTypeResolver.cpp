538b1f1041fccf3cb670b8f7654091b9e15cce7f
rn-santos227 <rn_santos227@yahoo.com>
2025-06-26 22:58:24 +0800

    updated component type resolver source file

--- src/factories/component/ComponentTypeResolver.cpp
+++ src/factories/component/ComponentTypeResolver.cpp
@@ -1,5 +1,8 @@
 #include "ComponentTypeResolver.h"
 
+#include <algorithm>
+#include <cctype>
+
 ComponentType ComponentTypeResolver::resolve(const std::string& name) {
   static const std::unordered_map<std::string, ComponentType> map = {
     {"GraphicsComponent", ComponentType::GRAPHICS},

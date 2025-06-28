a75a5b3d921d7e08115c12a99384e59178007da7
rn-santos227 <rn_santos227@yahoo.com>
2025-06-28 09:59:02 +0800

    created bounding box component header file

---
+++ src/components/bounding_box_component/BoundingBoxComponent.h
@@ -0,0 +1,12 @@
+#ifndef BOUNDING_BOX_COMPONENT_H
+#define BOUNDING_BOX_COMPONENT_H
+
+#include "components/BaseComponent.h"
+
+namespace Project::Components {
+  class BoundingBoxComponent : public BaseComponent {
+
+  };
+}
+
+#endif

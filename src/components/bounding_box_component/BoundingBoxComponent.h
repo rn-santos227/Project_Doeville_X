e9d341a438eaaf4792626bb24b98ae00be9034f1
rn-santos227 <rn_santos227@yahoo.com>
2025-06-28 10:57:51 +0800

    updated set solid function in bounding box component header file

--- src/components/bounding_box_component/BoundingBoxComponent.h
+++ src/components/bounding_box_component/BoundingBoxComponent.h
@@ -19,6 +19,8 @@ namespace Project::Components {
     void clearBoxes();
     const std::vector<SDL_Rect>& getBoxes() const;
 
+    void setSolid(bool solidEnabled);
+
   private:
     std::vector<SDL_Rect> boxes;
     int entityX = 0;

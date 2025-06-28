096ebf9d31251735cbe5af0554c99929fd262b1a
rn-santos227 <rn_santos227@yahoo.com>
2025-06-28 10:45:20 +0800

    updated bounding box component header file

--- src/components/bounding_box_component/BoundingBoxComponent.h
+++ src/components/bounding_box_component/BoundingBoxComponent.h
@@ -2,10 +2,18 @@
 #define BOUNDING_BOX_COMPONENT_H
 
 #include "components/BaseComponent.h"
+#include "components/PositionableComponent.h"
+
+#include <SDL.h>
+#include <vector>
 
 namespace Project::Components {
-  class BoundingBoxComponent : public BaseComponent {
+  class BoundingBoxComponent : public BaseComponent, public PositionableComponent {
+  public:
+    explicit BoundingBoxComponent(LogsManager& logsManager);
 
+  private:
+    std::vector<SDL_Rect> boxes;
   };
 }
 

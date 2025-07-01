865ec68d3bafea42597ac9460fd692d25ed308f5
rn-santos227 <rn_santos227@yahoo.com>
2025-07-01 20:55:26 +0800

    updated color utils header file in utilities

--- src/utilities/color/ColorUtils.h
+++ src/utilities/color/ColorUtils.h
@@ -1,4 +1,11 @@
 #ifndef COLOR_UTILS_H
 #define COLOR_UTILS_H
 
+#include <SDL.h>
+#include <string>
+
+namespace Project::Utilities {
+  SDL_Color hexToRGB(const std::string& hex, Uint8 alpha = 255);
+}
+
 #endif

e64a3a596893e94a3039b1c7625ce166a25c085b
rn-santos227 <rn_santos227@yahoo.com>
2025-07-02 20:19:17 +0800

    updated set color hex function in text component source file

--- src/components/text_component/TextComponent.cpp
+++ src/components/text_component/TextComponent.cpp
@@ -35,6 +35,7 @@ namespace Project::Components {
 
   void TextComponent::setColorHex(const std::string& hex) {
     color = Project::Utilities::hexToRGB(hex, color.a);
+    createTexture();
   }
 
   void TextComponent::setPosition(int x, int y) {

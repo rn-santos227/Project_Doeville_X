65a6496a0f766b168a8263dbfb65b0b00fef3306
rn-santos227 <rn_santos227@yahoo.com>
2025-07-03 22:28:53 +0800

    updated color utils header and source files

--- src/utilities/color/ColorUtils.cpp
+++ src/utilities/color/ColorUtils.cpp
@@ -6,6 +6,9 @@
 
 namespace Project::Utilities {
   SDL_Color ColorUtils::hexToRGB(const std::string& hex, Uint8 alpha) {
+    constexpr Uint8 SIXTEEN_BIT = 16;
+    constexpr Uint8 EIGHT_BIT = 8;
+
     std::string value = hex;
     if (!value.empty() && value[0] == '#') {
       value.erase(0, 1);
@@ -27,8 +30,8 @@ namespace Project::Utilities {
     ss >> rgb;
 
     SDL_Color color;
-    color.r = static_cast<Uint8>((rgb >> 16) & 0xFF);
-    color.g = static_cast<Uint8>((rgb >> 8) & 0xFF);
+    color.r = static_cast<Uint8>((rgb >> SIXTEEN_BIT) & 0xFF);
+    color.g = static_cast<Uint8>((rgb >> EIGHT_BIT) & 0xFF);
     color.b = static_cast<Uint8>(rgb & 0xFF);
     color.a = alpha;
     return color;
--- src/utilities/color/ColorUtils.h
+++ src/utilities/color/ColorUtils.h
@@ -4,10 +4,14 @@
 #include <SDL.h>
 #include <string>
 
+#include "libraries/constants/Constants.h"
+
+namespace Constants = Project::Libraries::Constants;
+
 namespace Project::Utilities {
   class ColorUtils {
   public:
-    static SDL_Color hexToRGB(const std::string& hex, Uint8 alpha = 255);
+    static SDL_Color hexToRGB(const std::string& hex, Uint8 alpha = Constants::FULL_ALPHA);
   };
 }
 

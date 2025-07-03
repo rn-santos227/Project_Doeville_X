a8a692d5b65491ad3ce8d0214e4297f2c1d2e3b7
rn-santos227 <rn_santos227@yahoo.com>
2025-07-03 19:59:53 +0800

    added libraries constants header file

---
+++ src/libraries/constants/Constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <SDL.h>
+#include <string>
+
+namespace Project::Libraries::Constants {
+  const std::string PROJECT_NAME = "Project Doeville X";
+  const std::string VERSION = "1.0.0";
+} 
+
+#endif 

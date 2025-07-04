a578dd506eecb635b40dfdfe7e50e598b950e130
rn-santos227 <rn_santos227@yahoo.com>
2025-07-05 01:23:13 +0800

    created debug display header file in screen handler folder

---
+++ src/handlers/screen/DebugDisplay.h
@@ -0,0 +1,20 @@
+#ifndef DEBUG_DISPLAY_H
+#define DEBUG_DISPLAY_H
+
+#include <SDL.h>
+#include <string>
+
+#include "core/SDLManager.h"
+#include "handlers/font/FontHandler.h"
+#include "handlers/input/MouseHandler.h"
+#include "libraries/constants/Constants.h"
+#include "utilities/config_reader/ConfigReader.h"
+#include "utilities/frames_counter/FramesCounter.h"
+#include "utilities/logs_manager/LogsManager.h"
+
+namespace Constants = Project::Libraries::Constants;
+
+namespace Project::Handlers {
+
+}
+#endif

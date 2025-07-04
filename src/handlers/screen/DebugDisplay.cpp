2935bc54b7e522458c94bc6612feae009110cc3e
rn-santos227 <rn_santos227@yahoo.com>
2025-07-05 01:30:05 +0800

    created debug display source file

---
+++ src/handlers/screen/DebugDisplay.cpp
@@ -0,0 +1,12 @@
+#include "DebugDisplay.h"
+
+namespace Project::Handlers {
+  using Project::Utilities::LogsManager;
+  using Project::Utilities::FramesCounter;
+  using Project::Utilities::ConfigReader;
+  using Project::Core::SDLManager;
+  using Project::Handlers::FontHandler;
+  using Project::Handlers::MouseHandler;
+  
+  namespace Constants = Project::Libraries::Constants;
+}

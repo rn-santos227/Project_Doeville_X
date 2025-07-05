dd76b961e66bd2ed52502dab68d87501b779e3b3
rn-santos227 <rn_santos227@yahoo.com>
2025-07-06 00:12:24 +0800

    updated render mouse position function in debug display source file

--- src/handlers/screen/DebugDisplay.cpp
+++ src/handlers/screen/DebugDisplay.cpp
@@ -79,5 +79,11 @@ namespace Project::Handlers {
 
     std::string mousePositionText = "Mouse: (" + std::to_string(mouseX) + ", " + std::to_string(mouseY) + ")";
     SDL_Texture* mouseTexture = fontHandler.renderText(renderer, mousePositionText, "system", debugTextColor);
+
+    if (mouseTexture) {
+
+    } else {
+      logsManager.logError("Failed to render mouse position text.");
+    }
   }
 }

7fa5acfeaa24c72ecacaadfaa13e5396bcb0f7ed
rn-santos227 <rn_santos227@yahoo.com>
2025-07-06 00:09:54 +0800

    updated render benchmark function in debug display source file

--- src/handlers/screen/DebugDisplay.cpp
+++ src/handlers/screen/DebugDisplay.cpp
@@ -55,5 +55,11 @@ namespace Project::Handlers {
     double frameTime = framesCounter.getDeltaTime() * Constants::MILLISECONDS_PER_SECOND;
     std::string benchmarkText = "Frame: " + std::to_string(static_cast<int>(frameTime)) + " ms";
     SDL_Texture* benchmarkTexture = fontHandler.renderText(renderer, benchmarkText, "system", debugTextColor);
+
+    if( benchmarkTexture) {
+
+    } else {
+      logsManager.logError("Failed to render benchmark text.");
+    }
   }
 }

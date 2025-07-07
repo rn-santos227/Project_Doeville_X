580da51add828552a7e3464c003a22f0c9f3f3eb
rn-santos227 <rn_santos227@yahoo.com>
2025-07-07 18:13:40 +0800

    created keys component header file

---
+++ src/components/keys_component/KeysComponent.h
@@ -0,0 +1,18 @@
+#ifndef KEYS_COMPONENT_H
+#define KEYS_COMPONENT_H
+
+#include "components/BaseComponent.h"
+
+#include <string>
+#include <unordered_map>
+
+#include "handlers/input/KeyHandler.h"
+
+namespace Project { namespace Entities { class Entity; } }
+
+namespace Project::Components {
+  class KeysComponent : public BaseComponent {
+    
+  };
+}
+#endif

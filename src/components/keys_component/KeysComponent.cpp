cc46a0e0d49969fe253448f0d60ec42fae289c9a
rn-santos227 <rn_santos227@yahoo.com>
2025-07-07 18:20:12 +0800

    added updated function in keys component source file

--- src/components/keys_component/KeysComponent.cpp
+++ src/components/keys_component/KeysComponent.cpp
@@ -7,6 +7,7 @@ namespace Project::Components {
   KeysComponent::KeysComponent(Project::Utilities::LogsManager& logsManager, KeyHandler* keyHandler)
     : BaseComponent(logsManager), keyHandler(keyHandler) {}
 
-  
-    
+  void KeysComponent::update(float deltaTime) {
+    if (!keyHandler || !owner) return;
+  }
 }

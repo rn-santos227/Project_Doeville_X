508fa11e165780a38f4fa27ed1f0b300c74d38e2
rn-santos227 <rn_santos227@yahoo.com>
2025-07-07 18:47:58 +0800

    updated calculate distance function in physics utils source file

--- src/utilities/physics/PhysicsUtils.cpp
+++ src/utilities/physics/PhysicsUtils.cpp
@@ -9,6 +9,8 @@ namespace Project::Utilities {
   }
 
   float PhysicsUtils::calculateDistance(float x1, float y1, float x2, float y2) {
-
+    float dx = x2 - x1;
+    float dy = y2 - y1;
+    return std::sqrt(dx * dx + dy * dy);
   }
 }

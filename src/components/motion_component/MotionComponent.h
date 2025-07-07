8106c9d0330831de1d2a3e057ba2c6522f242e1f
rn-santos227 <rn_santos227@yahoo.com>
2025-07-07 19:36:26 +0800

    updated motion component header file

--- src/components/motion_component/MotionComponent.h
+++ src/components/motion_component/MotionComponent.h
@@ -1,5 +1,24 @@
 #ifndef MOTION_COMPONENT_H
 #define MOTION_COMPONENT_H
 
+#include "components/BaseComponent.h"
+#include "components/PositionableComponent.h"
+#include "components/keys_component/KeysComponent.h"
+#include "handlers/input/KeyHandler.h"
+#include "libraries/constants/Constants.h"
 
+namespace Project { namespace Entities { class Entity; } }
+
+namespace Project::Components {
+  class MotionComponent : public BaseComponent {
+  public:
+    MotionComponent(Project::Utilities::LogsManager& logsManager, Project::Handlers::KeyHandler* keyHandler, float speed = Project::Libraries::Constants::DEFAULT_MOTION_SPEED);
+    ~MotionComponent() override = default;
+
+  private:
+    Project::Handlers::KeyHandler* keyHandler = nullptr;
+    Project::Entities::Entity* owner = nullptr;
+    float speed;
+  };
+}
 #endif

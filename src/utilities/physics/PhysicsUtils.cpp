ea658631aae732a1554c190953c1e724aa51b1ab
rn-santos227 <rn_santos227@yahoo.com>
2025-07-07 18:46:41 +0800

    updated check collision function in physics utils source file

--- src/utilities/physics/PhysicsUtils.cpp
+++ src/utilities/physics/PhysicsUtils.cpp
@@ -4,6 +4,7 @@
 
 namespace Project::Utilities {
   bool PhysicsUtils::checkCollision(const SDL_Rect& a, const SDL_Rect& b) {
-    
+    SDL_Rect intersection;
+    return SDL_IntersectRect(&a, &b, &intersection);
   }
 }

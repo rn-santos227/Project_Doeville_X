9862a0a3dabfeffabbe71cd2f3e92d63e253b071
rn-santos227 <rn_santos227@yahoo.com>
2025-07-07 19:39:23 +0800

    created motion component source file

---
+++ src/components/motion_component/MotionComponent.cpp
@@ -0,0 +1,9 @@
+#include "entities/Entity.h"
+#include "components/keys_component/KeysComponent.h"
+
+namespace Project::Components {
+  using Project::Handlers::KeyAction;
+  using Project::Handlers::KeyHandler;
+  using Project::Entities::Entity;
+
+}

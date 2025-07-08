f2671761d380e5305f5f7f9f6ac17817530d86bf
rn-santos227 <rn_santos227@yahoo.com>
2025-07-08 21:52:01 +0800

    added bounding box and graphics components in player entity

--- scripts/entities/player.entity.lua
+++ scripts/entities/player.entity.lua
@@ -3,7 +3,23 @@ y = 360
 z = 0
 
 components = {
-
+  BoundingBoxComponent = {
+    component = "BoundingBoxComponent",
+    active = true,
+    solid = false,
+    boxes = {
+      { x = 0, y = 0, w = 32, h = 32 }
+    }
+  },
+  
+  GraphicsComponent = {
+    component = "GraphicsComponent",
+    active = true,
+    width = 32,
+    height = 32,
+    colorHex = "ADD8E6",
+    colorA = 255
+  }
 }
 
 return {

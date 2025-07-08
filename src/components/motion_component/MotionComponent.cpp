ee9ccf61aaf384506fb032c4655ee3d2590566b1
rn-santos227 <rn_santos227@yahoo.com>
2025-07-08 20:34:00 +0800

    added constructor in motion component source file

--- src/components/motion_component/MotionComponent.cpp
+++ src/components/motion_component/MotionComponent.cpp
@@ -1,9 +1,13 @@
-#include "entities/Entity.h"
+#include "MotionComponent.h"
+
 #include "components/keys_component/KeysComponent.h"
 
 namespace Project::Components {
   using Project::Handlers::KeyAction;
   using Project::Handlers::KeyHandler;
   using Project::Entities::Entity;
+
+  MotionComponent::MotionComponent(Project::Utilities::LogsManager& logsManager, KeyHandler* keyHandler, float speed)
+      : BaseComponent(logsManager), keyHandler(keyHandler), speed(speed) {}
 
 }

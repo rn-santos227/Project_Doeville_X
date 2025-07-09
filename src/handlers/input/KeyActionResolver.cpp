90db6f5b3d532c0b284595cdc5e4b49b10592d5e
rn-santos227 <rn_santos227@yahoo.com>
2025-07-09 19:57:04 +0800

    updated resolve function in key action resoliver source file

--- src/handlers/input/KeyActionResolver.cpp
+++ src/handlers/input/KeyActionResolver.cpp
@@ -5,6 +5,11 @@
 
 namespace Project::Handlers {
   KeyAction KeyActionResolver::resolve(const std::string& name) {
-
+    static const std::unordered_map<std::string, KeyAction> map = {
+      {"MOVE_UP", KeyAction::MOVE_UP},
+      {"MOVE_DOWN", KeyAction::MOVE_DOWN},
+      {"MOVE_LEFT", KeyAction::MOVE_LEFT},
+      {"MOVE_RIGHT", KeyAction::MOVE_RIGHT},
+    };
   }
 }

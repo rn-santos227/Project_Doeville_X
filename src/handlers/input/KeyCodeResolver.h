a80a25cc38b64f4dcfda01ee7f42ac9499fd9fda
rn-santos227 <rn_santos227@yahoo.com>
2025-07-09 20:02:06 +0800

    created key code resolver header file in input handler

---
+++ src/handlers/input/KeyCodeResolver.h
@@ -0,0 +1,13 @@
+#ifndef KEY_CODE_RESOLVER_H
+#define KEY_CODE_RESOLVER_H
+
+#include <SDL2/SDL.h>
+#include <string>
+
+namespace Project::Handlers {
+  class KeyCodeResolver {
+
+  };
+}
+
+#endif

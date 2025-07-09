5a439e27bd37348f80052738e8672ad0f86b6dc4
rn-santos227 <rn_santos227@yahoo.com>
2025-07-09 20:04:25 +0800

    updated resolve function in key code resolver source file

--- src/handlers/input/KeyCodeResolver.cpp
+++ src/handlers/input/KeyCodeResolver.cpp
@@ -4,5 +4,10 @@
 namespace Project::Handlers {
   SDL_Scancode KeyCodeResolver::resolve(const std::string& name) {
     if (name.empty()) return SDL_SCANCODE_UNKNOWN;
+
+    SDL_Scancode code = SDL_GetScancodeFromName(name.c_str());
+    if (code >= SDL_SCANCODE_F1 && code <= SDL_SCANCODE_F12) {
+      return SDL_SCANCODE_UNKNOWN;
+    }
   }
 }

92554d078b96ac2a2a5b735078bb36e630dfd721
rn-santos227 <rn_santos227@yahoo.com>
2025-07-09 21:17:04 +0800

    updated components factory source file

--- src/factories/component/ComponentsFactory.cpp
+++ src/factories/component/ComponentsFactory.cpp
@@ -15,6 +15,7 @@ namespace Project::Factories {
   using Project::Utilities::LuaStateWrapper;
 
   namespace Constants = Project::Libraries::Constants;
+  namespace Keys = Project::Libraries::Keys;
 
   ComponentsFactory::ComponentsFactory(ConfigReader& configReader, LogsManager& logsManager, ResourcesHandler& resourcesHandler)
   : configReader(configReader), logsManager(logsManager), resourcesHandler(resourcesHandler), renderer(nullptr), keyHandler(nullptr) {}
@@ -24,7 +25,7 @@ namespace Project::Factories {
       return nullptr;
     }
 
-    std::string resolvedName = luaStateWrapper.getTableString(tableName, Project::Libraries::Keys::COMPONENT, componentName);
+    std::string resolvedName = luaStateWrapper.getTableString(tableName, Keys::COMPONENT, componentName);
     ComponentType type = ComponentTypeResolver::resolve(resolvedName);
     
     switch (type) {
@@ -46,28 +47,28 @@ namespace Project::Factories {
   // Components Builder Section
  std::unique_ptr<BaseComponent> ComponentsFactory::createBoundingBoxComponent(LuaStateWrapper& luaStateWrapper, const std::string& tableName) {
     SDL_Color defaultColor = Constants::DEFAULT_DEBUG_TEXT_COLOR;
-    SDL_Color debugColor = configReader.getColorValue(Project::Libraries::Keys::FONT_SECTION, Project::Libraries::Keys::FONT_DEFAULT_COLOR, defaultColor);
+    SDL_Color debugColor = configReader.getColorValue(Keys::FONT_SECTION, Keys::FONT_DEFAULT_COLOR, defaultColor);
     auto boxComponent = std::make_unique<BoundingBoxComponent>(logsManager, renderer, keyHandler, debugColor);
 
     lua_State* L = luaStateWrapper.get();
     lua_getglobal(L, tableName.c_str());
     if (lua_istable(L, -1)) {
-      lua_getfield(L, -1, Project::Libraries::Keys::BOXES);
+      lua_getfield(L, -1, Keys::BOXES);
       if (lua_istable(L, -1)) {
         lua_pushnil(L);
         while (lua_next(L, -2)) {
           if (lua_istable(L, -1)) {
             int x = 0, y = 0, w = 0, h = 0;
-            lua_getfield(L, -1, Project::Libraries::Keys::X);
+            lua_getfield(L, -1, Keys::X);
             if (lua_isnumber(L, -1)) x = static_cast<int>(lua_tonumber(L, -1));
             lua_pop(L, 1);
-            lua_getfield(L, -1, Project::Libraries::Keys::Y);
+            lua_getfield(L, -1, Keys::Y);
             if (lua_isnumber(L, -1)) y = static_cast<int>(lua_tonumber(L, -1));
             lua_pop(L, 1);
-            lua_getfield(L, -1, Project::Libraries::Keys::W);
+            lua_getfield(L, -1, Keys::W);
             if (lua_isnumber(L, -1)) w = static_cast<int>(lua_tonumber(L, -1));
             lua_pop(L, 1);
-            lua_getfield(L, -1, Project::Libraries::Keys::H);
+            lua_getfield(L, -1, Keys::H);
             if (lua_isnumber(L, -1)) h = static_cast<int>(lua_tonumber(L, -1));
             lua_pop(L, 1);
             SDL_Rect rect{x, y, w, h};
@@ -82,32 +83,32 @@ namespace Project::Factories {
       lua_pop(L, 1);
     }
 
-    bool active = luaStateWrapper.getTableBoolean(tableName, Project::Libraries::Keys::ACTIVE, true);
+    bool active = luaStateWrapper.getTableBoolean(tableName, Keys::ACTIVE, true);
     boxComponent->setActive(active);
 
-    bool solid = luaStateWrapper.getTableBoolean(tableName, Project::Libraries::Keys::SOLID, false);
+    bool solid = luaStateWrapper.getTableBoolean(tableName, Keys::SOLID, false);
     boxComponent->setSolid(solid);
 
     return boxComponent;
  }
 
   std::unique_ptr<BaseComponent> ComponentsFactory::createGraphicsComponent(LuaStateWrapper& luaStateWrapper, const std::string& tableName) {
-    std::string imagePath = luaStateWrapper.getTableString(tableName, Project::Libraries::Keys::TEXTURE_PATH, "");
+    std::string imagePath = luaStateWrapper.getTableString(tableName, Keys::TEXTURE_PATH, "");
 
     auto graphicsComponent = std::make_unique<GraphicsComponent>(renderer, logsManager);
 
     if (!imagePath.empty()) {
       graphicsComponent->setTexture(resourcesHandler, imagePath);
     } else {
-      int width = static_cast<int>(luaStateWrapper.getTableNumber(tableName, Project::Libraries::Keys::WIDTH, Project::Libraries::Constants::DEFAULT_COMPONENT_SIZE));
-      int height = static_cast<int>(luaStateWrapper.getTableNumber(tableName, Project::Libraries::Keys::HEIGHT, Project::Libraries::Constants::DEFAULT_COMPONENT_SIZE));
-      std::string colorHex = luaStateWrapper.getTableString(tableName, Project::Libraries::Keys::COLOR_HEX, Project::Libraries::Constants::DEFAULT_SHAPE_COLOR_HEX);
-      Uint8 alpha = static_cast<Uint8>(luaStateWrapper.getTableNumber(tableName, Project::Libraries::Keys::COLOR_ALPHA, Project::Libraries::Constants::FULL_ALPHA));
+      int width = static_cast<int>(luaStateWrapper.getTableNumber(tableName, Keys::WIDTH, Constants::DEFAULT_COMPONENT_SIZE));
+      int height = static_cast<int>(luaStateWrapper.getTableNumber(tableName, Keys::HEIGHT, Constants::DEFAULT_COMPONENT_SIZE));
+      std::string colorHex = luaStateWrapper.getTableString(tableName, Keys::COLOR_HEX, Constants::DEFAULT_SHAPE_COLOR_HEX);
+      Uint8 alpha = static_cast<Uint8>(luaStateWrapper.getTableNumber(tableName, Keys::COLOR_ALPHA, Constants::FULL_ALPHA));
       SDL_Color color = ColorUtils::hexToRGB(colorHex, alpha);
       graphicsComponent->setShape(width, height, color);
     }
 
-    bool active = luaStateWrapper.getTableBoolean(tableName, Project::Libraries::Keys::ACTIVE, true);
+    bool active = luaStateWrapper.getTableBoolean(tableName, Keys::ACTIVE, true);
     graphicsComponent->setActive(active);
 
     graphicsComponent->onAttach();
@@ -115,17 +116,17 @@ namespace Project::Factories {
   }
 
   std::unique_ptr<BaseComponent> ComponentsFactory::createTextComponent(LuaStateWrapper& luaStateWrapper, const std::string& tableName) {
-    std::string colorHex = luaStateWrapper.getTableString(tableName, Project::Libraries::Keys::FONT_COLOR_HEX, Project::Libraries::Constants::DEFAULT_SHAPE_COLOR_HEX);
-    Uint8 alpha = static_cast<Uint8>(luaStateWrapper.getTableNumber(tableName, Project::Libraries::Keys::FONT_COLOR_ALPHA, Project::Libraries::Constants::FULL_ALPHA));
+    std::string colorHex = luaStateWrapper.getTableString(tableName, Keys::FONT_COLOR_HEX, Constants::DEFAULT_SHAPE_COLOR_HEX);
+    Uint8 alpha = static_cast<Uint8>(luaStateWrapper.getTableNumber(tableName, Keys::FONT_COLOR_ALPHA, Constants::FULL_ALPHA));
     SDL_Color color = ColorUtils::hexToRGB(colorHex, alpha);
 
-    std::string text = luaStateWrapper.getTableString(tableName, Project::Libraries::Keys::TEXT, Project::Libraries::Keys::DEFAULT_TEXT);
+    std::string text = luaStateWrapper.getTableString(tableName, Keys::TEXT, Keys::DEFAULT_TEXT);
 
-    std::string defaultFontPath = configReader.getValue(Project::Libraries::Keys::FONT_SECTION, Project::Libraries::Keys::FONT_DEFAULT_PATH, Project::Libraries::Constants::DEFAULT_FONT_PATH);
-    std::string fontPath = luaStateWrapper.getTableString(tableName, Project::Libraries::Keys::FONT_PATH, defaultFontPath);
+    std::string defaultFontPath = configReader.getValue(Keys::FONT_SECTION, Keys::FONT_DEFAULT_PATH, Keys::DEFAULT_FONT_PATH);
+    std::string fontPath = luaStateWrapper.getTableString(tableName, Keys::FONT_PATH, defaultFontPath);
 
-    int defaultFontSize = configReader.getIntValue(Project::Libraries::Keys::FONT_SECTION, Project::Libraries::Keys::FONT_DEFAULT_SIZE, Project::Libraries::Constants::DEFAULT_FONT_SIZE);
-    int fontSize = static_cast<int>(luaStateWrapper.getTableNumber(tableName, Project::Libraries::Keys::FONT_SIZE, static_cast<float>(defaultFontSize)));
+    int defaultFontSize = configReader.getIntValue(Keys::FONT_SECTION, Keys::FONT_DEFAULT_SIZE, Constants::DEFAULT_FONT_SIZE);
+    int fontSize = static_cast<int>(luaStateWrapper.getTableNumber(tableName, Keys::FONT_SIZE, static_cast<float>(defaultFontSize)));
 
     TTF_Font* font = TTF_OpenFont(fontPath.c_str(), fontSize);
     if (logsManager.checkAndLogError(font == nullptr, "Failed to load font: " + fontPath)) {
@@ -133,7 +134,7 @@ namespace Project::Factories {
     }
 
     auto textComponent = std::make_unique<TextComponent>(renderer, font, color, text, fontPath, fontSize, logsManager);
-    bool active = luaStateWrapper.getTableBoolean(tableName, Project::Libraries::Keys::ACTIVE, true);
+    bool active = luaStateWrapper.getTableBoolean(tableName, Keys::ACTIVE, true);
     textComponent->setActive(active);
     return textComponent;
   }
@@ -144,8 +145,8 @@ namespace Project::Factories {
   }
 
   SDL_Color ComponentsFactory::getLuaSDLColor(LuaStateWrapper& luaStateWrapper) {
-    std::string hex = luaStateWrapper.getGlobalString(Project::Libraries::Keys::FONT_COLOR_HEX, Project::Libraries::Constants::DEFAULT_SHAPE_COLOR_HEX);
-    Uint8 alpha = getLuaColorChannel(luaStateWrapper, Project::Libraries::Keys::FONT_COLOR_ALPHA);
+    std::string hex = luaStateWrapper.getGlobalString(Keys::FONT_COLOR_HEX, Constants::DEFAULT_SHAPE_COLOR_HEX);
+    Uint8 alpha = getLuaColorChannel(luaStateWrapper, Keys::FONT_COLOR_ALPHA);
     return ColorUtils::hexToRGB(hex, alpha);
   }
 

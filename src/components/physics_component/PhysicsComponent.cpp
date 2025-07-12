4a86697beea4a9a5b48e5cffa380e8bbd447505f
rn-santos227 <rn_santos227@yahoo.com>
2025-07-12 13:12:25 +0800

    updated physics component header file

--- src/components/physics_component/PhysicsComponent.cpp
+++ src/components/physics_component/PhysicsComponent.cpp
@@ -4,5 +4,11 @@
 #include "entities/Entity.h"
 
 namespace Project::Components {
+  PhysicsComponent::PhysicsComponent(Project::Utilities::LogsManager& logsManager)
+    : BaseComponent(logsManager) {}
 
+  void PhysicsComponent::update(float deltaTime) {
+    velocityX += accelerationX * deltaTime;
+    velocityY += accelerationY * deltaTime;
+  }
 }

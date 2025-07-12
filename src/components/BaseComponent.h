7b0896ec231df62339bda8eed10a838f021e5c70
rn-santos227 <rn_santos227@yahoo.com>
2025-07-13 01:03:56 +0800

    updated base component header file

--- src/components/BaseComponent.h
+++ src/components/BaseComponent.h
@@ -1,18 +1,20 @@
 #ifndef BASE_COMPONENT_H
 #define BASE_COMPONENT_H
 
+#include "interfaces/build_interface/Buildable.h"
 #include "interfaces/render_interface/Renderable.h"
 #include "interfaces/update_interface/Updatable.h"
 #include "utilities/logs_manager/LogsManager.h"
 
 namespace Project::Components {
-  class BaseComponent : public Project::Interfaces::Renderable, public Project::Interfaces::Updatable {
+  class BaseComponent : public Project::Interfaces::Buildable, public Project::Interfaces::Renderable, public Project::Interfaces::Updatable {
   public:
     explicit BaseComponent(Project::Utilities::LogsManager& logsManager) : logsManager(logsManager) {}
     virtual ~BaseComponent() = default;
 
     virtual void update(float deltaTime) = 0;
     virtual void render() = 0;
+    virtual void build(Project::Utilities::LuaStateWrapper& luaStateWrapper, const std::string& tableName) = 0;
 
     virtual void onAttach() {}
     virtual void onDetach() {}

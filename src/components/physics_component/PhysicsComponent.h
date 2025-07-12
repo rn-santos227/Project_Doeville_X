5f2c70311fb4f6bc3a8dbca81dc203d9f2603ca9
rn-santos227 <rn_santos227@yahoo.com>
2025-07-12 13:08:30 +0800

    created physics component header fle

---
+++ src/components/physics_component/PhysicsComponent.h
@@ -0,0 +1,15 @@
+#ifndef PHYSICS_COMPONENT_H
+#define PHYSICS_COMPONENT_H
+
+#include "components/BaseComponent.h"
+#include "libraries/constants/Constants.h"
+
+namespace Project { namespace Entities { class Entity; } }
+
+namespace Project::Components {
+  class PhysicsComponent : public BaseComponent {
+
+  };
+}
+
+#endif

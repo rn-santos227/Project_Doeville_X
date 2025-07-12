ed04b472dc667fa9620e41dbf4415cc33edadfb3
rn-santos227 <rn_santos227@yahoo.com>
2025-07-12 13:12:35 +0800

    updated physics component header file

--- src/components/physics_component/PhysicsComponent.cpp
+++ src/components/physics_component/PhysicsComponent.cpp
@@ -10,5 +10,23 @@ namespace Project::Components {
   void PhysicsComponent::update(float deltaTime) {
     velocityX += accelerationX * deltaTime;
     velocityY += accelerationY * deltaTime;
+
+    if (friction > 0.0f) {
+      float decel = friction * deltaTime;
+      if (velocityX > 0.0f) {
+        velocityX -= decel;
+        if (velocityX < 0.0f) velocityX = 0.0f;
+      } else if (velocityX < 0.0f) {
+        velocityX += decel;
+        if (velocityX > 0.0f) velocityX = 0.0f;
+      }
+      if (velocityY > 0.0f) {
+        velocityY -= decel;
+        if (velocityY < 0.0f) velocityY = 0.0f;
+      } else if (velocityY < 0.0f) {
+        velocityY += decel;
+        if (velocityY > 0.0f) velocityY = 0.0f;
+      }
+    }
   }
 }

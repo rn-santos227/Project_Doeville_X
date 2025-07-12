524d241491917a1252e31237e5380452cae013a5
rn-santos227 <rn_santos227@yahoo.com>
2025-07-12 13:09:14 +0800

    updated physics component header fle

--- src/components/physics_component/PhysicsComponent.h
+++ src/components/physics_component/PhysicsComponent.h
@@ -8,7 +8,17 @@ namespace Project { namespace Entities { class Entity; } }
 
 namespace Project::Components {
   class PhysicsComponent : public BaseComponent {
+  public:
 
+  private:
+    Project::Entities::Entity* owner = nullptr;
+    
+    float velocityX = 0.0f;
+    float velocityY = 0.0f;
+    float accelerationX = 0.0f;
+    float accelerationY = 0.0f;
+    float friction = Project::Libraries::Constants::DEFAULT_FRICTION;
+    float restitution = Project::Libraries::Constants::DEFAULT_BOUNCE_FACTOR;
   };
 }
 

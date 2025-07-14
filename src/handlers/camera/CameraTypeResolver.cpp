d5edefae39b80abd2ccd61650de261b110687323
rn-santos227 <rn_santos227@yahoo.com>
2025-07-14 21:20:41 +0800

    created camera type resolver source file

---
+++ src/handlers/camera/CameraTypeResolver.cpp
@@ -0,0 +1,14 @@
+#include "CameraTypeResolver.h"
+
+#include <algorithm>
+#include <cctype>
+#include <unordered_map>
+
+#include "libraries/categories/Categories.h"
+
+namespace Project::Handlers {
+  namespace Cameras = Project::Libraries::Categories::Cameras;
+  CameraType CameraTypeResolver::resolve(const std::string& name) {
+
+  };
+}

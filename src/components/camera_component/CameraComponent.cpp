05cbb498ba419c3afe5cbc3ce2fdbdddb27b4fcd
rn-santos227 <rn_santos227@yahoo.com>
2025-07-14 21:35:23 +0800

    created camera components header source in components

---
+++ src/components/camera_component/CameraComponent.cpp
@@ -0,0 +1,23 @@
+#include "CameraComponent.h"
+
+#include "entities/Entity.h"
+#include "libraries/keys/Keys.h"
+
+namespace Project::Components {
+  namespace Keys = Project::Libraries::Keys;
+
+  CameraComponent::CameraComponent(Project::Utilities::LogsManager& logsManager, Project::Handlers::CameraHandler* handler)
+    : BaseComponent(logsManager), cameraHandler(handler) {}
+
+  void CameraComponent::update(float /*deltaTime*/) {
+    if (!owner || !cameraHandler) return;
+    int camX = static_cast<int>(owner->getX() - cameraHandler->getWidth() / 2);
+    int camY = static_cast<int>(owner->getY() - cameraHandler->getHeight() / 2);
+    cameraHandler->setPosition(camX, camY);
+  }
+
+  void CameraComponent::build(Project::Utilities::LuaStateWrapper& luaStateWrapper, const std::string& tableName) {
+    bool active = luaStateWrapper.getTableBoolean(tableName, Keys::ACTIVE, true);
+    setActive(active);
+  }
+}

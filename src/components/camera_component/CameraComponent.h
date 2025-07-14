9a1c83959291027b84d4f15803f6ec56658ef033
rn-santos227 <rn_santos227@yahoo.com>
2025-07-14 21:32:33 +0800

    created camera components header file in components

---
+++ src/components/camera_component/CameraComponent.h
@@ -0,0 +1,28 @@
+#ifndef CAMERA_COMPONENT_H
+#define CAMERA_COMPONENT_H
+
+#include "components/BaseComponent.h"
+#include "handlers/camera/CameraHandler.h"
+
+namespace Project { namespace Entities { class Entity; } }
+
+namespace Project::Components {
+  class CameraComponent : public BaseComponent {
+  public:
+    CameraComponent(Project::Utilities::LogsManager& logsManager, Project::Handlers::CameraHandler* handler);
+    ~CameraComponent() override = default;
+
+    void update(float deltaTime) override;
+    void render() override {}
+    void build(Project::Utilities::LuaStateWrapper& luaStateWrapper, const std::string& tableName) override;
+
+    void setEntityReference(Project::Entities::Entity* entity) { owner = entity; }
+    void setCameraHandler(Project::Handlers::CameraHandler* handler) { cameraHandler = handler; }
+
+  private:
+    Project::Entities::Entity* owner = nullptr;
+    Project::Handlers::CameraHandler* cameraHandler = nullptr;
+  };
+}
+
+#endif

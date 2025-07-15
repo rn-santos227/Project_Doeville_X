be06cc917480986b25673f1b618b778a3ec997a0
rn-santos227 <rn_santos227@yahoo.com>
2025-07-15 21:22:33 +0800

    added magnitude function in math utils source file

--- src/utilities/math/MathUtils.cpp
+++ src/utilities/math/MathUtils.cpp
@@ -10,4 +10,9 @@ namespace Project::Utilities {
   float MathUtils::cross(float x1, float y1, float x2, float y2) {
     return x1 * y2 - y1 * x2;
   }
+
+  float MathUtils::magnitude(float x, float y) {
+    return std::sqrt(x * x + y * y);
+  }
+
 }

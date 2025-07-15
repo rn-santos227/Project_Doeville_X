8ce9cbe5db9b76496430ef1c90df9d2f20f18730
rn-santos227 <rn_santos227@yahoo.com>
2025-07-15 21:42:49 +0800

    updated entity seeder header file

--- src/entities/EntitySeeder.h
+++ src/entities/EntitySeeder.h
@@ -22,6 +22,18 @@ namespace Project::Entities {
     EntitiesManager& manager;
     Project::Factories::EntitiesFactory& factory;
 
+    std::vector<std::string> entityTemplates;
+    std::vector<std::string> spawnedIds;
+
+    std::mt19937 rng;
+    std::uniform_real_distribution<float> dist{-1.0f, 1.0f};
+
+    float spawnRadius = 300.0f;
+    size_t maxEntities = 10;
+    size_t idCounter = 0;
+
+    void spawnEntity();
+    float distanceSquared(float x1, float y1, float x2, float y2) const;
   };
 }
 

ace5c48aeac0d29e580e72e28da6ed0160585989
rn-santos227 <rn_santos227@yahoo.com>
2025-07-15 21:18:35 +0800

    updated math utils header file in utilities

--- src/utilities/math/mathUtils.h
+++ src/utilities/math/mathUtils.h
@@ -6,7 +6,10 @@
 namespace Project::Utilities {
   class MathUtils {
   public:
-
+    static float dot(float x1, float y1, float x2, float y2);
+    static float cross(float x1, float y1, float x2, float y2);
+    static float magnitude(float x, float y);
+    static SDL_FPoint normalize(float x, float y);
   };
 }
 

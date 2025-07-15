f49fd95437aa336f2bc9e930b7bc079b886d92e3
rn-santos227 <rn_santos227@yahoo.com>
2025-07-15 22:46:23 +0800

    updated entity seeder source file

--- src/entities/EntitySeeder.cpp
+++ src/entities/EntitySeeder.cpp
@@ -8,8 +8,20 @@
 #include "factories/entity/EntitiesFactory.h"
 #include "libraries/constants/Constants.h"
 #include "libraries/keys/Keys.h"
+#include "utilities/math/MathUtils.h"
 
 namespace Project::Entities {
-  
+  using Project::Utilities::MathUtils;
+
+  EntitySeeder::EntitySeeder(EntitiesManager& mgr, Project::Factories::EntitiesFactory& fac)
+  : manager(mgr), factory(fac) {
+    std::random_device rd;
+    rng.seed(rd());
+  }
+
+  void EntitySeeder::setPlayer(std::shared_ptr<Entity> p) {
+    player = p;
+  }
+
 }
 

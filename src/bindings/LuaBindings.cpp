2fd08af8e717073bf3031d701846f4b11b092a0c
rn-santos227 <rn_santos227@yahoo.com>
2025-07-17 19:39:26 +0800

    updated lua set background image function in lua bindings source file

--- src/bindings/LuaBindings.cpp
+++ src/bindings/LuaBindings.cpp
@@ -17,6 +17,9 @@ namespace Project::Bindings::LuaBindings {
   using Project::Entities::Entity;
 
   int lua_setBackgroundImage(lua_State* L) {
-
+    GameState* state = static_cast<GameState*>(lua_touserdata(L, lua_upvalueindex(1)));
+    if (!state) {
+      return luaL_error(L, "Invalid GameState reference in lua_setBackgroundImage.");
+    }
   }
 }

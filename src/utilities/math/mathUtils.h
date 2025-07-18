8d0216a4ae86929630d74f61c4857014daefb878
rn-santos227 <rn_santos227@yahoo.com>
2025-07-18 23:12:45 +0800

    updated math utils header file

--- src/utilities/math/mathUtils.h
+++ src/utilities/math/mathUtils.h
@@ -6,10 +6,14 @@
 namespace Project::Utilities {
   class MathUtils {
   public:
-    static float dot(float x1, float y1, float x2, float y2);
+    static float angleBetween(float x1, float y1, float x2, float y2);
     static float cross(float x1, float y1, float x2, float y2);
+    static float dot(float x1, float y1, float x2, float y2);
+    static float lerp(float a, float b, float t);
     static float magnitude(float x, float y);
+    
     static SDL_FPoint normalize(float x, float y);
+    static SDL_FPoint rotate(float x, float y, float angleDeg);
   };
 }
 

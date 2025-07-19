428308a2aac591016ba7bc6936f668879de9e95f
rn-santos227 <rn_santos227@yahoo.com>
2025-07-19 20:52:45 +0800

    created style mmaanger header file

--- src/services/styling/Style.h
+++ src/services/styling/Style.h
@@ -6,12 +6,15 @@
 namespace Project::Services {
   struct Style {
     SDL_Color background{0, 0, 0, 0};
+    SDL_Color borderColor{0, 0, 0, 0};
     SDL_Color foreground{0, 0, 0, 0};
     SDL_Color gradientStart{0, 0, 0, 0};
     SDL_Color gradientEnd{0, 0, 0, 0};
 
     int margin{0};
     int padding{0};
+    int fontSize{0};
+    int fontWeight{0};
     
     float radius{0.0f};
   };
---
+++ src/services/styling/StyleManager.h
@@ -0,0 +1,10 @@
+#ifndef STYLE_MANAGER_H
+#define STYLE_MANAGER_H
+
+namespace Project::Services {
+  class StyleManager {
+
+  };
+}
+
+#endif

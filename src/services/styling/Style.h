9743a29e704d1be38ce97261c4f0cc975a4b8ad6
rn-santos227 <rn_santos227@yahoo.com>
2025-07-19 20:31:56 +0800

    updated style header file in styling services folder

--- src/services/styling/Style.h
+++ src/services/styling/Style.h
@@ -5,10 +5,15 @@
 
 namespace Project::Services {
   struct Style {
+    SDL_Color background{0, 0, 0, 0};
+    SDL_Color foreground{0, 0, 0, 0};
     SDL_Color gradientStart{0, 0, 0, 0};
     SDL_Color gradientEnd{0, 0, 0, 0};
+
     int margin{0};
     int padding{0};
+    
+    float radius{0.0f};
   };
 }
 

2227d20812500611fa92bc3a90bc8ad1ad919f0f
rn-santos227 <rn_santos227@yahoo.com>
2025-07-19 20:54:39 +0800

    updated style manager header file

--- src/services/styling/StyleManager.h
+++ src/services/styling/StyleManager.h
@@ -1,9 +1,18 @@
 #ifndef STYLE_MANAGER_H
 #define STYLE_MANAGER_H
 
+#include "Style.h"
+
+#include <string>
+#include <unordered_map>
+
 namespace Project::Services {
   class StyleManager {
 
+
+  private:
+    StyleManager() = default;
+    std::unordered_map<std::string, Style> styles;
   };
 }
 

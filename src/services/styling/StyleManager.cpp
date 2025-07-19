8de348a362c1ead20adefbc173dfd0be768926f9
rn-santos227 <rn_santos227@yahoo.com>
2025-07-19 21:11:41 +0800

    updated style manager source file

--- src/services/styling/StyleManager.cpp
+++ src/services/styling/StyleManager.cpp
@@ -16,5 +16,11 @@ namespace Project::Services {
     std::regex ruleRegex("(\\.[\\w-]+)\\s*\\{([^}]+)\\}");
     std::smatch match;
     std::string::const_iterator searchStart(css.cbegin());
+
+    while (std::regex_search(searchStart, css.cend(), match, ruleRegex)) {
+      std::string selector = match[1].str();
+      std::string body = match[2].str();
+      Style style;
+    }
   }
 }

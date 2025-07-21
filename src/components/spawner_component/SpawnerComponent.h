a88ad46abecf3ec1dfedd0ccd98f3c44173eb2a0
rn-santos227 <rn_santos227@yahoo.com>
2025-07-21 14:44:50 +0800

    created spawner component header file

---
+++ src/components/spawner_component/SpawnerComponent.h
@@ -0,0 +1,15 @@
+#ifndef SPAWNER_COMPONENT_H
+#define SPAWNER_COMPONENT_H
+
+#include "components/BaseComponent.h"
+
+#include <lua.hpp>
+#include <string>
+
+namespace Project { namespace Entities { class Entity; } }
+
+namespace Project::Components {
+
+}
+
+#endif

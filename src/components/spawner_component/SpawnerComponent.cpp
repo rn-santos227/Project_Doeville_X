7aff8f556057d53649428028dc7ea05548086cf3
rn-santos227 <rn_santos227@yahoo.com>
2025-07-21 15:04:33 +0800

    created spawner component source file

---
+++ src/components/spawner_component/SpawnerComponent.cpp
@@ -0,0 +1,11 @@
+#include "SpawnerComponent.h"
+
+#include "entities/Entity.h"
+#include "entities/EntitiesManager.h"
+#include "factories/entity/EntitiesFactory.h"
+#include "states/GameState.h"
+#include "libraries/keys/Keys.h"
+
+namespace Project::Components {
+
+}

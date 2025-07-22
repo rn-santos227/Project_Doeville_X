f12d2c2fb5eaf3557e82cc6d727f50d2472a70ce
rn-santos227 <rn_santos227@yahoo.com>
2025-07-22 18:09:11 +0800

    updated main file in root src folder

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,21 @@
 
 #include <iostream>
 #include <SDL.h> 
+
 #include "core/GameEngine.h"
+#include "utilities/exception/EngineException.h"
 
 int main(int argc, char* argv[]) { 
   Project::Core::GameEngine engine;
   
-  engine.init();  
-  engine.run();  
+  try {
+    engine.init();
+    engine.run();
+  } catch (const Project::Utilities::EngineException& e) {
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Engine Error", e.what(), nullptr);
+  } catch (const std::exception& e) {
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Unexpected Error", e.what(), nullptr);
+  }
   
   return 0;
 }

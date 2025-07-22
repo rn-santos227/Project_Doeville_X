6f72e053eb770f657d6e5e33ec9ad33ef24abcbc
rn-santos227 <rn_santos227@yahoo.com>
2025-07-22 14:52:24 +0800

    implemented reset interface in game state manager header file

--- src/states/GameStateManager.h
+++ src/states/GameStateManager.h
@@ -12,12 +12,17 @@
 #include "entities/EntitiesManager.h"
 #include "interfaces/cleanup_interface/Cleanable.h"
 #include "interfaces/render_interface/Renderable.h"
+#include "interfaces/reset_interface/Resetable.h"
 #include "interfaces/update_interface/Updatable.h"
 #include "helpers/objects_manager/ObjectsManager.h"
 #include "utilities/logs_manager/LogsManager.h"
 
 namespace Project::States {
-  class GameStateManager : public Project::Interfaces::Cleanable, public Project::Interfaces::Renderable, public Project::Interfaces::Updatable {
+  class GameStateManager : 
+  public Project::Interfaces::Cleanable,
+  public Project::Interfaces::Renderable,
+  public Project::Interfaces::Resetable,
+  public Project::Interfaces::Updatable {
   public:
     GameStateManager(size_t cacheLimit, Project::Utilities::LogsManager& logsManager);
     ~GameStateManager() = default;
@@ -33,6 +38,7 @@ namespace Project::States {
     
     void update(float deltaTime);
     void render();
+    void reset();
 
     void enableStates(const std::vector<std::string>& names);
     void disableStates(const std::vector<std::string>& names);
@@ -51,9 +57,9 @@ namespace Project::States {
     std::list<std::pair<std::string, std::unique_ptr<GameState>>> stateCache;
     std::unordered_map<std::string, decltype(stateCache.begin())> cacheMap;
 
-    std::stack<GameState*> stateStack;
-
     std::shared_ptr<Project::Entities::EntitiesManager> globalEntitiesManager = std::make_shared<Project::Entities::EntitiesManager>();
+    std::stack<GameState*> stateStack;
+    std::string initialStateName;
     
     void addToCache(const std::string& name, std::unique_ptr<GameState> state);
     GameState* retrieveFromCache(const std::string& name);

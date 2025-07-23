650373b83d19ffe75c2a4166ce21052b2e63d52a
rn-santos227 <rn_santos227@yahoo.com>
2025-07-23 19:06:26 +0800

    added constructor in button component header file

--- src/components/button_component/ButtonComponent.h
+++ src/components/button_component/ButtonComponent.h
@@ -15,13 +15,27 @@ namespace Project { namespace Handlers { class MouseHandler; class CursorHandler
 namespace Project::Components {
   class ButtonComponent : public BaseComponent, public PositionableComponent, public Project::Interfaces::Stylable {
   public:
-
+    ButtonComponent(
+      SDL_Renderer* renderer,
+      Project::Utilities::LogsManager& logsManager,
+      Project::Handlers::MouseHandler* mouseHandler,
+      Project::Handlers::CursorHandler* cursorHandler
+    );
   
   private:
+    SDL_Renderer* renderer = nullptr;
+
+    Project::Entities::Entity* owner = nullptr;
     Project::Handlers::CursorHandler* cursorHandler = nullptr;
     Project::Handlers::MouseHandler* mouseHandler = nullptr;
-
     
+    SDL_Rect rect{0,0,0,0};
+    SDL_Color color{255,255,255,255};
+    SDL_Color hoverColor{255,255,255,255};
+
+    std::string luaFunction;
+    bool hovered = false;
+    bool wasPressed = false;
   };
 }
 

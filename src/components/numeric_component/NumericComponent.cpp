8ba124ee951e49c6aa4b5f9d222342cc01ff7939
rn-santos227 <rn_santos227@yahoo.com>
2025-07-24 03:17:26 +0800

    updated build function in numeric component source file

--- src/components/numeric_component/NumericComponent.cpp
+++ src/components/numeric_component/NumericComponent.cpp
@@ -15,5 +15,10 @@ namespace Project::Components {
   void NumericComponent::build(Project::Utilities::LuaStateWrapper& luaStateWrapper, const std::string& tableName) {
     lua_State* L = luaStateWrapper.get();
     lua_getglobal(L, tableName.c_str());
+    if (lua_istable(L, -1)) {
+
+    } else {
+      lua_pop(L, 1);
+    }
   }
 }

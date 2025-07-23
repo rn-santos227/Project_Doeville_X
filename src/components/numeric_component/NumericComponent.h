d398bf637dcf68942e6898fa19ad1ceca42000c8
rn-santos227 <rn_santos227@yahoo.com>
2025-07-24 02:53:06 +0800

    created numeric component header file in components folder

---
+++ src/components/numeric_component/NumericComponent.h
@@ -0,0 +1,17 @@
+#ifndef NUMERIC_COMPONENT_H
+#define NUMERIC_COMPONENT_H
+
+#include <string>
+#include <unordered_map>
+
+#include "components/BaseComponent.h"
+
+namespace Project { namespace Entities { class Entity; } }
+
+namespace Project::Components {
+  class NumericComponent : public BaseComponent {
+
+  };
+}
+
+#endif

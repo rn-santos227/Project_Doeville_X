9ad8a036636ec843fe30c4c9573df86e3c7eae75
rn-santos227 <rn_santos227@yahoo.com>
2025-07-25 09:38:32 +0800

    created entity pool header file in helpers folder

---
+++ src/helpers/entity_pool/EntityPool.h
@@ -0,0 +1,14 @@
+#ifndef ENTITY_POOL_H
+#define ENTITY_POOL_H
+
+#include <functional>
+#include <memory>
+#include <vector>
+
+#include "entities/Entity.h"
+
+namespace Project::Helpers {
+
+}
+
+#endif

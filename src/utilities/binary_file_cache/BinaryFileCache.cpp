411594eaf37aa1ae1a54ad16589485fd1e23f6d7
rn-santos227 <rn_santos227@yahoo.com>
2025-07-26 11:19:19 +0800

    added get timestamp  function binary file cache source file

--- src/utilities/binary_file_cache/BinaryFileCache.cpp
+++ src/utilities/binary_file_cache/BinaryFileCache.cpp
@@ -10,4 +10,9 @@ namespace Project::Utilities {
   static long long toSeconds(fs::file_time_type tp) {
     return std::chrono::duration_cast<std::chrono::seconds>(tp.time_since_epoch()).count();
   }
+
+  long long BinaryFileCache::getTimestamp(const std::string& path) {
+    if (!fs::exists(path)) return 0;
+    return toSeconds(fs::last_write_time(path));
+  }
 }

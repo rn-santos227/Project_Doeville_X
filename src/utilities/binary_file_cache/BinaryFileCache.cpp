013f8ce1d1cdfaafe8859abb5a791701e5e7ae16
rn-santos227 <rn_santos227@yahoo.com>
2025-07-26 11:18:28 +0800

    added to seconds function binary file cache source file

--- src/utilities/binary_file_cache/BinaryFileCache.cpp
+++ src/utilities/binary_file_cache/BinaryFileCache.cpp
@@ -6,4 +6,8 @@
 
 namespace Project::Utilities {
   namespace fs = std::filesystem;
+
+  static long long toSeconds(fs::file_time_type tp) {
+    return std::chrono::duration_cast<std::chrono::seconds>(tp.time_since_epoch()).count();
+  }
 }

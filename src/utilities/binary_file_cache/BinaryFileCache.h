121516ee19202cb88d0789f80e5626402117a113
rn-santos227 <rn_santos227@yahoo.com>
2025-07-26 11:14:53 +0800

    created binary file cache header file for general cache storage

---
+++ src/utilities/binary_file_cache/BinaryFileCache.h
@@ -0,0 +1,27 @@
+#ifndef BINARY_FILE_CACHE_H
+#define BINARY_FILE_CACHE_H
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace Project::Utilities {
+  class BinaryFileCache {
+  public:
+    explicit BinaryFileCache(const std::string& filePath);
+
+    void load();
+    void save() const;
+
+    bool getData(const std::string& path, std::vector<char>& outData) const;
+    void setData(const std::string& path, const std::vector<char>& data);
+
+  private:
+    static long long getTimestamp(const std::string& path);
+
+    std::string cacheFilePath;
+    std::unordered_map<std::string, std::pair<long long, std::vector<char>>> cache;
+  };
+}
+
+#endif

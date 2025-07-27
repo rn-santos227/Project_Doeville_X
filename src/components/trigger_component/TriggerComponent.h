302582ed6f1434d8528aaca9fabd421445d524fd
rn-santos227 <rn_santos227@yahoo.com>
2025-07-27 19:07:13 +0800

    created trigger component header file

---
+++ src/components/trigger_component/TriggerComponent.h
@@ -0,0 +1,17 @@
+#ifndef TRIGGER_COMPONENT_H
+#define TRIGGER_COMPONENT_H
+
+#include "components/BaseComponent.h"
+
+#include <string>
+#include <vector>
+
+namespace Project { namespace Entities { class Entity; } }
+
+namespace Project::Components {
+  class TriggerComponent : public BaseComponent {
+
+  };
+}
+
+#endif

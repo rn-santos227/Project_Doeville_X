541d2aae3b8b42401ced11ba30ddb8529ed63bbe
rn-santos227 <rn_santos227@yahoo.com>
2025-07-29 20:17:20 +0800

    created motion component source file

---
+++ src/components/motion_component/MotionFunctions.cpp
@@ -0,0 +1,13 @@
+#include "MotionComponent.h"
+#include "MotionFunctions.h"
+
+#include <cmath>
+
+#include "components/bounding_box_component/BoundingBoxComponent.h"
+#include "components/physics_component/PhysicsComponent.h"
+#include "libraries/constants/Constants.h"
+#include "utilities/math/MathUtils.h"
+
+namespace Project::Components {
+
+}

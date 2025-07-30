cb9c3b34e048b0f6aedb5a2279f0b1973193617b
rn-santos227 <rn_santos227@yahoo.com>
2025-07-30 20:31:01 +0800

    updated input component header file

--- src/components/input_component/InputComponent.h
+++ src/components/input_component/InputComponent.h
@@ -13,6 +13,7 @@
 #include "interfaces/style_interface/Stylable.h"
 #include "handlers/input/MouseHandler.h"
 #include "handlers/input/CursorHandler.h"
+#include "libraries/constants/Constants.h"
 #include "utilities/logs_manager/LogsManager.h"
 #include "utilities/config_reader/ConfigReader.h"
 
@@ -22,15 +23,45 @@ namespace Project::Components {
     public PositionableComponent,
     public TextureHolder, 
     public Project::Interfaces::Stylable {
-    public:
-      InputComponent(
-        SDL_Renderer* renderer,
-        Project::Utilities::ConfigReader& configReader,
-        Project::Utilities::LogsManager& logsManager,
-        Project::Handlers::MouseHandler* mouseHandler,
-        Project::Handlers::CursorHandler* cursorHandler
-      );
-      ~InputComponent() override;
+  public:
+    InputComponent(
+      SDL_Renderer* renderer,
+      Project::Utilities::ConfigReader& configReader,
+      Project::Utilities::LogsManager& logsManager,
+      Project::Handlers::MouseHandler* mouseHandler,
+      Project::Handlers::CursorHandler* cursorHandler
+    );
+    ~InputComponent() override;
+
+    void update(float deltaTime) override;
+    void render() override;
+    void build(Project::Utilities::LuaStateWrapper& luaStateWrapper, const std::string& tableName) override;
+    void applyStyle() override;
+
+  private:
+    SDL_Renderer* renderer;
+    Project::Utilities::ConfigReader& configReader;
+    Project::Handlers::MouseHandler* mouseHandler;
+    Project::Handlers::CursorHandler* cursorHandler;
+
+    std::array<Uint8, SDL_NUM_SCANCODES> prevKeys{};
+
+    TTF_Font* font;
+    SDL_Rect rect{0,0,0,0};
+    SDL_Color bgColor{0,0,0,255};
+    SDL_Color textColor = Project::Libraries::Constants::COLOR_WHITE;
+    SDL_Color borderColor{0,0,0,0};
+
+    std::string fontPath;
+    std::string currentText;
+    std::string placeholder;
+
+    float cursorBlink{0.0f};
+
+    int fontSize = Project::Libraries::Constants::DEFAULT_BASE_FONT_SIZE;
+    int borderWidth{0};
+    int textureW{0};
+    int textureH{0};
   };
 }
 

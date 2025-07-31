6fe63c27c9e3a05d9636be88419d030c65520534
rn-santos227 <rn_santos227@yahoo.com>
2025-07-31 17:20:39 +0800

    updated resolve function in dimension mode resolver source file

--- src/states/DimensionModeResolver.cpp
+++ src/states/DimensionModeResolver.cpp
@@ -8,6 +8,16 @@
 namespace Project::States {
   namespace Dimensions = Project::Libraries::Modes::Dimensions;
   DimensionMode DimensionModeResolver::resolve(const std::string& name) {
-
+    static const std::unordered_map<std::string, DimensionMode> map = {
+      {std::string(Dimensions::BOXED), DimensionMode::BOXED},
+      {std::string(Dimensions::BOUNDED), DimensionMode::BOUNDED},
+      {std::string(Dimensions::FREE_ROAMING), DimensionMode::FREE_ROAMING},
+      {std::string(Dimensions::HORIZONTAL_SCROLL), DimensionMode::HORIZONTAL_SCROLL},
+      {std::string(Dimensions::ISOMETRIC), DimensionMode::ISOMETRIC},
+      {std::string(Dimensions::LOOPING), DimensionMode::LOOPING},
+      {std::string(Dimensions::THREE_DIMENSION), DimensionMode::THREE_DIMENSION},
+      {std::string(Dimensions::VERTICAL_SCROLL), DimensionMode::VERTICAL_SCROLL},
+      {std::string(Dimensions::WRAPPING), DimensionMode::WRAPPING}
+    };
   }
 }

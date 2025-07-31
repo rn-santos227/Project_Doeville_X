0330e036228561e90064a170dadab4076fc242a9
rn-santos227 <rn_santos227@yahoo.com>
2025-07-31 17:15:22 +0800

    created dimension mode resolver source file in states folder

---
+++ src/states/DimensionModeResolver.cpp
@@ -0,0 +1,13 @@
+#include "DimensionModeResolver.h"
+
+#include <cctype>
+#include <unordered_map>
+
+#include "libraries/modes/DimensionModes.h"
+
+namespace Project::States {
+  namespace Dimensions = Project::Libraries::Modes::Dimensions;
+  DimensionMode DimensionModeResolver::resolve(const std::string& name) {
+
+  }
+}

6ce87635249367f4fda3eff92888e0f2e96c91c1
rn-santos227 <rn_santos227@yahoo.com>
2025-07-31 17:04:52 +0800

    created dimension mode header file in states folder

---
+++ src/states/DimensionMode.h
@@ -0,0 +1,18 @@
+#ifndef DIMENSION_MODE_H
+#define DIMENSION_MODE_H
+
+namespace Project::States {
+  enum class DimensionMode {
+    BOXED,
+    BOUNDED,
+    FREE_ROAMING,
+    HORIZONTAL_SCROLL,
+    ISOMETRIC,
+    LOOPING,
+    THREE_DIMENSION,
+    VERTICAL_SCROLL,
+    WRAPPING
+  };
+}
+
+#endif

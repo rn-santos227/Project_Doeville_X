a8c7d6ea4e8360e0ef41c63b80f7b146242f7940
rn-santos227 <rn_santos227@yahoo.com>
2025-08-01 23:29:54 +0800

    udpated env loader header file in utilities folder

---
+++ src/utilities/env_loader/EnvLoader.h
@@ -0,0 +1,27 @@
+#ifndef ENV_LOADER_H
+#define ENV_LOADER_H
+
+#include <string>
+#include <unordered_map>
+
+#include "utilities/logs_manager/LogsManager.h"
+
+namespace Project::Utilities {
+  class EnvLoader {
+  public:
+    EnvLoader(Project::Utilities::LogsManager& logsManager, const std::string& filePath);
+
+    bool loadEnv();
+
+    std::string getValue(const std::string& key, const std::string& defaultValue = "") const;
+    const std::unordered_map<std::string, std::string>& getAll() const;
+
+  private:
+    Project::Utilities::LogsManager& logsManager;
+    std::unordered_map<std::string, std::string> values;
+
+    void trim(std::string& str) const;
+  };
+}
+
+#endif

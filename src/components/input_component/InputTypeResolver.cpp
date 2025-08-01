654863977fe1d9b8a657855e98083dec3f232992
rn-santos227 <rn_santos227@yahoo.com>
2025-08-01 22:30:31 +0800

    created input type resolver source file

---
+++ src/components/input_component/InputTypeResolver.cpp
@@ -0,0 +1,23 @@
+#include "InputTypeResolver.h"
+
+#include <array>
+#include <string_view>
+
+#include "libraries/categories/InputCategories.h"
+#include "utilities/string/StringUtils.h"
+
+namespace Project::Components {
+  namespace Inputs = Project::Libraries::Categories::Inputs;
+  InputType InputTypeResolver::resolve(std::string_view name) {
+    static constexpr std::array<std::pair<std::string_view, InputType>, 3> map{{
+      {Inputs::TEXT, InputType::TEXT},
+      {Inputs::NUMERIC, InputType::NUMERIC},
+      {Inputs::PASSWORD, InputType::PASSWORD}
+    }};
+
+    for (const auto& [key, value] : map) {
+      if (Project::Utilities::StringUtils::iequals(key, name)) return value;
+    }
+    return InputType::TEXT;
+  }
+}

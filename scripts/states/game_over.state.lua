ad286d675aaf83e93c9a93dc3f6c8186338e3ad7
rn-santos227 <rn_santos227@yahoo.com>
2025-08-02 00:12:12 +0800

    added game over state in lua states folder

---
+++ scripts/states/game_over.state.lua
@@ -0,0 +1,23 @@
+state_name = "GameOverState"
+state_category = "GAME_OVER_STATE"
+dimension = "BOXED"
+
+function initialize()
+  setBackgroundColor(0, 0, 0, 255)
+  spawnEntity("game_over_text")
+  spawnEntity("button_main_menu")
+end
+
+function onEnter()
+  print("Entering GameOverState")
+end
+
+function onExit()
+  print("Exit GameOverState")
+end
+
+function update(deltaTime)
+end
+
+function render()
+end

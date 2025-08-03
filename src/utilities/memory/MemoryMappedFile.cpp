fe51b87f8977b56f94e9b30df1a4690132d15219
rn-santos227 <rn_santos227@yahoo.com>
2025-08-03 19:22:03 +0800

    added constructor in memory mapped file source file

--- src/utilities/memory/MemoryMappedFile.cpp
+++ src/utilities/memory/MemoryMappedFile.cpp
@@ -8,11 +8,15 @@
 #endif
 
 namespace Project::Utilities {
-    MemoryMappedFile::MemoryMappedFile(const std::string& path)
+  MemoryMappedFile::MemoryMappedFile(const std::string& path)
 #ifdef _WIN32
-    : file(INVALID_HANDLE_VALUE), mapping(nullptr), fileSize(0), mapped(nullptr) {
-
+  : file(INVALID_HANDLE_VALUE), mapping(nullptr), fileSize(0), mapped(nullptr) {
+    file = CreateFileA(path.c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr,
+      OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+    if (file == INVALID_HANDLE_VALUE) {
+      return;
     }
+  }
 #else
 
 #endif

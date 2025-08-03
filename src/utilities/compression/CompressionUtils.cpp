0f15c12957756188fc44b2f4708dfe8936b851ae
rn-santos227 <rn_santos227@yahoo.com>
2025-08-03 19:09:24 +0800

    added decompress function in compress utils source file

--- src/utilities/compression/CompressionUtils.cpp
+++ src/utilities/compression/CompressionUtils.cpp
@@ -12,4 +12,21 @@ namespace Project::Utilities {
     dest.resize(destLen);
     return true;
   }
+
+  bool CompressionUtils::decompress(const unsigned char* src, size_t srcSize, std::vector<unsigned char>& dest) {
+    uLongf destLen = srcSize * 4 + 1024;
+    int res;
+    do {
+      dest.resize(destLen);
+      res = ::uncompress(dest.data(), &destLen, src, srcSize);
+      if (res == Z_BUF_ERROR) {
+        destLen *= 2;
+      }
+    } while (res == Z_BUF_ERROR);
+    if (res != Z_OK) {
+      return false;
+    }
+    dest.resize(destLen);
+    return true;
+  }
 }

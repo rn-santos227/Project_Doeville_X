e11bed4ecf9dea5b798913f6c78c8cad2a7e51fd
rn-santos227 <rn_santos227@yahoo.com>
2025-08-04 20:49:11 +0800

    updated lua camera zoon out function component source file

--- src/bindings/LuaBindings.cpp
+++ src/bindings/LuaBindings.cpp
@@ -347,6 +347,9 @@ namespace Project::Bindings::LuaBindings {
   int lua_cameraZoomOut(lua_State* L) {
     GameState* state = static_cast<GameState*>(lua_touserdata(L, lua_upvalueindex(Constants::INDEX_ONE)));
     float amount = static_cast<float>(luaL_optnumber(L, Constants::INDEX_ONE, Constants::DEFAULT_CAMERA_ZOOM_STEP));
+    if (!state || !state->getActiveCamera()) {
+      return 0;
+    }
   }
 
   int lua_setBackgroundColor(lua_State* L) {

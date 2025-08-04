cce96d214dc98d9f9ae3edd590151394983049e3
rn-santos227 <rn_santos227@yahoo.com>
2025-08-04 20:50:28 +0800

    updated lua shake camera function component source file

--- src/bindings/LuaBindings.cpp
+++ src/bindings/LuaBindings.cpp
@@ -357,6 +357,9 @@ namespace Project::Bindings::LuaBindings {
   int lua_cameraShake(lua_State* L) {
     GameState* state = static_cast<GameState*>(lua_touserdata(L, lua_upvalueindex(Constants::INDEX_ONE)));
     float duration = static_cast<float>(luaL_optnumber(L, Constants::INDEX_ONE, Constants::DEFAULT_CAMERA_SHAKE_DURATION));
+    if (!state || !state->getActiveCamera()) {
+      return 0;
+    }
   }
 
   int lua_setBackgroundColor(lua_State* L) {

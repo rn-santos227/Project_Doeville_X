f2293c558fb64e900112b05effdbdea48be02dae
rn-santos227 <rn_santos227@yahoo.com>
2025-08-05 15:04:25 +0800

    created meter component source file

---
+++ src/components/meter_component/MeterComponent.cpp
@@ -0,0 +1,20 @@
+#include "MeterComponent.h"
+
+#include <sstream>
+#include <algorithm>
+#include <cmath>
+
+#include "components/numeric_component/NumericComponent.h"
+#include "entities/Entity.h"
+#include "entities/EntitiesManager.h"
+#include "libraries/categories/ComponentCategories.h"
+#include "libraries/constants/Constants.h"
+#include "libraries/keys/Keys.h"
+#include "services/styling/StyleManager.h"
+#include "utilities/color/ColorUtils.h"
+#include "utilities/math/MathUtils.h"
+#include "utilities/geometry/GeometryUtils.h"
+
+namespace Project::Components {
+
+}

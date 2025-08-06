0c9579a939095b1de9fcdee72fc021e43a331388
rn-santos227 <rn_santos227@yahoo.com>
2025-08-06 21:56:51 +0800

    updated text data header file

--- src/components/text_component/TextData.h
+++ src/components/text_component/TextData.h
@@ -6,15 +6,23 @@
 namespace Project::Components {
   struct TextData {
     SDL_Rect rect{0, 0, 0, 0};
+    SDL_Rect textRect{0, 0, 0, 0};
 
     SDL_Color textColor = Project::Libraries::Constants::COLOR_WHITE;
     SDL_Color borderColor = Project::Libraries::Constants::COLOR_BLACK;
+    SDL_Color bgColor{0, 0, 0, 0};
 
     std::string currentText;
     std::string fontPath;
-    
+
     int fontSize = 0;
     int borderWidth = 0;
+    int borderRadius = 0;
+
+    int paddingTop{0};
+    int paddingRight{0};
+    int paddingBottom{0};
+    int paddingLeft{0};
   };
 }
 

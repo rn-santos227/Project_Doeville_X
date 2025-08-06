c8a6457a7a338b2b853228d9ddf36e4363036b5a
rn-santos227 <rn_santos227@yahoo.com>
2025-08-06 21:58:33 +0800

    updated render function in text component source file

--- src/components/text_component/TextComponent.cpp
+++ src/components/text_component/TextComponent.cpp
@@ -68,7 +68,12 @@ namespace Project::Components {
       SDL_SetRenderDrawColor(renderer, data.borderColor.r, data.borderColor.g, data.borderColor.b, data.borderColor.a);
       for (int i = 0; i < data.borderWidth; ++i) {
         SDL_Rect bRect = {data.rect.x + i, data.rect.y + i, data.rect.w - Constants::INDEX_TWO * i, data.rect.h - Constants::INDEX_TWO * i};
-        SDL_RenderDrawRect(renderer, &bRect);
+        if (data.borderRadius > 0) {
+          int radius = std::max(0, data.borderRadius - i);
+          Project::Utilities::GeometryUtils::renderRoundedRect(renderer, bRect, radius);
+        } else {
+          SDL_RenderDrawRect(renderer, &bRect);
+        }
       }
     }
   }

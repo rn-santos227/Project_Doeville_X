c3cefcc435b7023ee15ee4eb5bfd2fb0191b5bf6
rn-santos227 <rn_santos227@yahoo.com>
2025-08-07 22:11:13 +0800

    created asset manager header file

---
+++ src/assets/AssetManager.h
@@ -0,0 +1,20 @@
+#ifndef ASSETS_MANAGER_H
+#define ASSETS_MANAGER_H
+
+#include "BaseAsset.h"
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+
+#include "helpers/objects_manager/ObjectsManager.h"
+
+namespace Project::Assets {
+  class AssetsManager : public Project::Helpers::ObjectsManager<BaseAsset, std::unique_ptr<BaseAsset>> {
+  };
+
+}
+
+#endif

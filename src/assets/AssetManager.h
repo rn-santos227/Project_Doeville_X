a0fb28a224d32a6038f72ae5e415de25b6caa116
rn-santos227 <rn_santos227@yahoo.com>
2025-08-07 22:22:41 +0800

    added tag map and category in asset manager header file

--- src/assets/AssetManager.h
+++ src/assets/AssetManager.h
@@ -13,8 +13,13 @@
 
 namespace Project::Assets {
   class AssetsManager : public Project::Helpers::ObjectsManager<BaseAsset, std::unique_ptr<BaseAsset>> {
+  public:
     AssetsManager() = default;
     ~AssetsManager() = default;
+
+  private:
+    std::unordered_map<std::string, std::vector<std::string>> tagMap;
+    std::unordered_map<AssetCategory, std::vector<std::string>> categoryMap;
   };
 
 }

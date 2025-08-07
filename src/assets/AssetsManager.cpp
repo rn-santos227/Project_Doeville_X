8d10c2de2bbc852dac3711bb4ee761b6a76ceb73
rn-santos227 <rn_santos227@yahoo.com>
2025-08-07 22:30:16 +0800

    updated add asset function in assets maanger source file

--- src/assets/AssetsManager.cpp
+++ src/assets/AssetsManager.cpp
@@ -1,8 +1,12 @@
 #include "AssetsManager.h"
 
 namespace Project::Assets {
+  using Project::Helpers::ObjectsManager;
+
   void AssetsManager::addAsset(const std::string& id, std::unique_ptr<BaseAsset> asset) {
     if (!asset) return;
     tagMap[asset->getTag()].push_back(id);
+    categoryMap[asset->getCategory()].push_back(id);
+    ObjectsManager<BaseAsset, std::unique_ptr<BaseAsset>>::add(id, std::move(asset));
   }
 }

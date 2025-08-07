3e5bb3f2a1cf424036a64e009726bf3be3e6cf74
rn-santos227 <rn_santos227@yahoo.com>
2025-08-07 22:39:52 +0800

    created assets factory header file in factories folder

---
+++ src/factories/asset/AssetsFactory.h
@@ -0,0 +1,18 @@
+#ifndef ASSETS_FACTORY_H
+#define ASSETS_FACTORY_H
+
+#include <memory>
+#include <string>
+#include <SDL2/SDL.h>
+
+#include "assets/AssetsManager.h"
+#include "handlers/resources/ResourcesHandler.h"
+#include "utilities/logs_manager/LogsManager.h"
+
+namespace Project::Factories {
+  class AssetsFactory {
+
+  };
+}
+
+#endif

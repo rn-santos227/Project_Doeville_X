f7516b38cc230891a46c1105db06b20ac776b6fb
rn-santos227 <rn_santos227@yahoo.com>
2025-08-08 21:00:15 +0800

    created texture asset header file

---
+++ src/assets/texture_asset/TextureAsset.h
@@ -0,0 +1,19 @@
+#ifndef TEXTURE_ASSET_H
+#define TEXTURE_ASSET_H
+
+#include "TextureAssetData.h"
+
+#include <string>
+#include <SDL.h>
+
+#include "assets/BaseAsset.h"
+#include "utilities/lua_state_wrapper/LuaStateWrapper.h"
+
+
+namespace Project::Assets {
+  class TextureAsset : public BaseAsset {
+
+  };
+}
+
+#endif

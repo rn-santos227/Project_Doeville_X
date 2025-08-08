8048152540381218bcdfa8df087b6a693fac301b
rn-santos227 <rn_santos227@yahoo.com>
2025-08-08 21:33:17 +0800

    created texture asset source file

---
+++ src/assets/texture_asset/TextureAsset.cpp
@@ -0,0 +1,12 @@
+#include "TextureAsset.h"
+
+#include "libraries/keys/LuaAssetKeys.h"
+#include "utilities/color_shader/ColorShader.h"
+
+namespace Project::Assets {
+  using Project::Utilities::LuaStateWrapper;
+  using Project::Libraries::Keys::LUA_ASSET_GET_ASSET;
+  
+  namespace Keys = Project::Libraries::Keys;
+
+}

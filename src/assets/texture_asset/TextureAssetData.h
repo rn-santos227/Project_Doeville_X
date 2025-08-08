e467583251f2d78cc2f63c05b7b86c16b7557f4b
rn-santos227 <rn_santos227@yahoo.com>
2025-08-08 21:31:24 +0800

    udpated texture asset data header file

--- src/assets/texture_asset/TextureAssetData.h
+++ src/assets/texture_asset/TextureAssetData.h
@@ -1,13 +1,19 @@
 #ifndef TEXTURE_ASSET_DATA_H
 #define TEXTURE_ASSET_DATA_H
 
+#include <SDL.h>
+
+#include "libraries/constants/ColorConstants.h"
 #include "libraries/constants/FloatConstants.h"
 
 namespace Project::Assets {
   struct TextureAssetData {
+    SDL_Color color = Project::Libraries::Constants::COLOR_WHITE;
+
+    float scale = Project::Libraries::Constants::DEFAULT_WHOLE;
+
     int width{0};
     int height{0};
-    float scale = Project::Libraries::Constants::DEFAULT_WHOLE;
   };
 }
 

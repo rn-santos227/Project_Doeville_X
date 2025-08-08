f04e15af7501717fa82f475a00d5e86980e8164d
rn-santos227 <rn_santos227@yahoo.com>
2025-08-08 21:35:23 +0800

    added constructor in texture asset source file

--- src/assets/texture_asset/TextureAsset.cpp
+++ src/assets/texture_asset/TextureAsset.cpp
@@ -4,9 +4,15 @@
 #include "utilities/color_shader/ColorShader.h"
 
 namespace Project::Assets {
+  using Project::Utilities::LogsManager;
   using Project::Utilities::LuaStateWrapper;
+  using Project::Handlers::ResourcesHandler;
   using Project::Libraries::Keys::LUA_ASSET_GET_ASSET;
   
   namespace Keys = Project::Libraries::Keys;
+
+  TextureAsset::TextureAsset(SDL_Renderer* renderer, LogsManager& logsManager, ResourcesHandler& resourcesHandler)
+    : BaseAsset(renderer, logsManager, resourcesHandler),
+      texture(nullptr) {}
 
 }

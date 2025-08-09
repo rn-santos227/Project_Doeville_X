05ed6406dbfaf8d9fba1045abc9a412f4d48cf41
rn-santos227 <rn_santos227@yahoo.com>
2025-08-09 22:44:50 +0800

    created map asset source file

---
+++ src/assets/map_asset/MapAsset.cpp
@@ -0,0 +1,15 @@
+#include "MapAsset.h"
+
+#include <fstream>
+#include <sstream>
+
+#include "libraries/keys/LuaAssetKeys.h"
+#include "utilities/lua_state_wrapper/LuaStateWrapper.h"
+
+namespace Project::Assets {
+  using Project::Utilities::LogsManager;
+  using Project::Utilities::LuaStateWrapper;
+  using Project::Handlers::ResourcesHandler;
+
+  namespace Keys = Project::Libraries::Keys;
+}

61d916fdff51280e7ccefbb0cc3b65d456bc79e5
rn-santos227 <rn_santos227@yahoo.com>
2025-08-09 22:13:42 +0800

    created tile handler header file in handlers folder

--- src/assets/tile_asset/TileAsset.h
+++ src/assets/tile_asset/TileAsset.h
@@ -26,8 +26,8 @@ namespace Project::Assets {
     ) override;
 
     SDL_Texture* getTexture() const { return data.texture; }
-    SDL_Rect getTileRect(char id) const;
-    bool isTilePassable(char id) const;
+    SDL_Rect getTileRect(char id) const  { return tileData.rect; }
+    bool isTilePassable(char id) const { return tileData.passable; }
     
     int getTileWidth() const { return tileData.width; }
     int getTileHeight() const { return tileData.height; }
---
+++ src/handlers/tile/TileHandler.h
@@ -0,0 +1,15 @@
+#ifndef TILE_HANDLER_H
+#define TILE_HANDLER_H
+
+#include <string>
+#include <vector>
+#include <SDL.h>
+
+#include "assets/AssetsManager.h"
+#include "utilities/logs_manager/LogsManager.h"
+
+namespace Project::Handlers {
+
+}
+
+#endif

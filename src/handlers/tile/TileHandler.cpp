1475d87057c92d01d1b04e9f4f9acc65670b2a98
rn-santos227 <rn_santos227@yahoo.com>
2025-08-10 17:41:33 +0800

    updated build map function in tile handler source file

--- src/handlers/tile/TileHandler.cpp
+++ src/handlers/tile/TileHandler.cpp
@@ -4,7 +4,7 @@
 
 namespace Project::Handlers {
   using Project::Utilities::LogsManager;
-  using Project::Assets::TileAsset;
+  using Project::Assets::MapAsset;
   using Project::Assets::TileAsset;
   using Project::Assets::AssetsManager;
 
@@ -12,6 +12,12 @@ namespace Project::Handlers {
     : renderer(renderer), assetsManager(assetsManager), logsManager(logsManager) {}
 
   std::vector<BuiltTile> TileHandler::buildMap(const std::string& mapAssetId) {
-
+    std::vector<BuiltTile> tilesOut;
+    auto* base = assetsManager.getAsset(mapAssetId);
+    auto* mapAsset = dynamic_cast<MapAsset*>(base);
+    if (!mapAsset) {
+      logsManager.logError("Map asset not found: " + mapAssetId);
+      return tilesOut;
+    }
   }
 }

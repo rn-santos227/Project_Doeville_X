8645202b2ae258cc6bf7c0c6774f0dd91ed3bca8
rn-santos227 <rn_santos227@yahoo.com>
2025-08-12 21:39:32 +0800

    updated motion system source file

--- src/systems/motion_system/MotionSystem.cpp
+++ src/systems/motion_system/MotionSystem.cpp
@@ -4,6 +4,7 @@
 #include "libraries/constants/NumericConstants.h"
 #include "libraries/constants/ProfileConstants.h"
 #include "utilities/profiler/Profiler.h"
+#include "utilities/thread/ThreadPool.h"
 
 namespace Project::Systems {
   using Project::Components::MotionComponent;
@@ -22,11 +23,22 @@ namespace Project::Systems {
 
   void Project::Systems::MotionSystem::update(float deltaTime) {
     PROFILE_SCOPE(Project::Libraries::Constants::MOTION_PROFILE);
-    for (auto* comp : components) {
-      if (comp && comp->isActive()) {
-        comp->update(deltaTime);
-      }
+    auto& pool = Project::Utilities::ThreadPool::getInstance();
+    const size_t chunk = 64;
+    const size_t total = components.size();
+    for (size_t i = 0; i < total; i += chunk) {
+      size_t begin = i;
+      size_t end = std::min(i + chunk, total);
+      pool.enqueue([this, begin, end, deltaTime]() {
+        for (size_t j = begin; j < end; ++j) {
+          auto* comp = components[j];
+          if (comp && comp->isActive()) {
+            comp->update(deltaTime);
+          }
+        }
+      });
     }
+    pool.wait();
   }
 
   void Project::Systems::MotionSystem::clear() {

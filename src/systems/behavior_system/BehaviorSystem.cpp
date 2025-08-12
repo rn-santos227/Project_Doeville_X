42a85215f20807025388372715db96984a561e98
rn-santos227 <rn_santos227@yahoo.com>
2025-08-12 21:36:47 +0800

    created behavior system source file in systems folder

---
+++ src/systems/behavior_system/BehaviorSystem.cpp
@@ -0,0 +1,8 @@
+#include "BehaviorSystem.h"
+
+namespace Project::Systems {
+  void BehaviorSystem::update(float /*deltaTime*/) {
+    PROFILE_SCOPE(Project::Libraries::Constants::BEHAVIOR_PROFILE);
+    // Placeholder behavior update job
+  }
+}

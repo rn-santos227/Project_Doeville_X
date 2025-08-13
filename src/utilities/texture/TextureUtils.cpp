fba5b40ac7b5064e7fee9f7caf5d6e61a910aca6
rn-santos227 <rn_santos227@yahoo.com>
2025-08-13 21:25:56 +0800

    updated select mip function in texture utils soruce file

--- src/utilities/texture/TextureUtils.cpp
+++ src/utilities/texture/TextureUtils.cpp
@@ -14,6 +14,22 @@ namespace Project::Utilities::TextureUtils {
   }
 
   SDL_Surface* selectMip(SDL_Surface* surface, int maxDimension) {
-
+    if (!surface) return nullptr;
+    int width = surface->w;
+    int height = surface->h;
+    SDL_Surface* current = surface;
+    while ((width > maxDimension || height > maxDimension) && width > 1 && height > 1) {
+      int newW = width / Project::Libraries::Constants::INDEX_TWO;
+      int newH = height / Project::Libraries::Constants::INDEX_TWO;
+      SDL_Surface* scaled = SDL_CreateRGBSurfaceWithFormat(0, newW, newH, surface->format->BitsPerPixel, surface->format->format);
+      if (!scaled) break;
+      SDL_Rect dst{0, 0, newW, newH};
+      SDL_BlitScaled(current, nullptr, scaled, &dst);
+      if (current != surface) SDL_FreeSurface(current);
+      current = scaled;
+      width = newW;
+      height = newH;
+    }
+    return current;
   }
 }

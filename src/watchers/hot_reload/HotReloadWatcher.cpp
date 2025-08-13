6dae69ec3d725efd3888cbf72e884e612ad47bbe
rn-santos227 <rn_santos227@yahoo.com>
2025-08-13 21:39:20 +0800

    updated watch file function in hot reload watcher source

--- src/watchers/hot_reload/HotReloadWatcher.cpp
+++ src/watchers/hot_reload/HotReloadWatcher.cpp
@@ -11,7 +11,16 @@ namespace Project::Watchers {
   }
 
   void HotReloadWatcher::watchFile(const std::string& path, std::function<void()> onChange) {
-
+    std::lock_guard<std::mutex> lock(watchMutex);
+    std::filesystem::file_time_type ts;
+    if (std::filesystem::exists(path)) {
+      ts = std::filesystem::last_write_time(path);
+    }
+    watches.push_back({path, ts, onChange});
+    if (!running) {
+      running = true;
+      worker = std::thread(&HotReloadWatcher::threadFunc, this);
+    }
   }
 
   void HotReloadWatcher::stop() {

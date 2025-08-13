c5a683b0b4f86e8a428dd028b53ad7f473c09e17
rn-santos227 <rn_santos227@yahoo.com>
2025-08-13 20:43:29 +0800

    created memory budget tracker header file in memory utilities folder

---
+++ src/utilities/memory/MemoryBudgetTracker.h
@@ -0,0 +1,14 @@
+#ifndef MEMORY_BUDGET_TRACKER_H
+#define MEMORY_BUDGET_TRACKER_H
+
+#include <cstddef>
+#include <unordered_map>
+
+#include "utilities/logs_manager/LogsManager.h"
+#include "utilities/profiler/Profiler.h"
+
+namespace Project::Utilities {
+  
+}
+
+#endif

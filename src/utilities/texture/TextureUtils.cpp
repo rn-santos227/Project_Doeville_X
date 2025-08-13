e87ebe1606d0bbb5837ebc97a503c6156982df75
rn-santos227 <rn_santos227@yahoo.com>
2025-08-13 21:25:14 +0800

    updated compress function in texture utils soruce file

--- src/utilities/texture/TextureUtils.cpp
+++ src/utilities/texture/TextureUtils.cpp
@@ -1,5 +1,19 @@
 #include "TextureUtils.h"
 
-namespace Project::Utilities::TextureUtils {
+#include "libraries/constants/IndexConstants.h"
 
+namespace Project::Utilities::TextureUtils {
+  SDL_Surface* compress(SDL_Surface* surface) {
+    if (!surface) return nullptr;
+    SDL_Surface* converted = SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_RGBA32, 0);
+    if (converted && converted != surface) {
+      SDL_FreeSurface(surface);
+      surface = converted;
+    }
+    return surface;
+  }
+
+  SDL_Surface* selectMip(SDL_Surface* surface, int maxDimension) {
+
+  }
 }

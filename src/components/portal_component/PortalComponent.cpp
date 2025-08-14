b2045de14d8d346e48af379b05f7a9a143ffd09c
rn-santos227 <rn_santos227@yahoo.com>
2025-08-14 21:31:08 +0800

    created portal component source file

---
+++ src/components/portal_component/PortalComponent.cpp
@@ -0,0 +1,14 @@
+#include "PortalComponent.h"
+
+#include <SDL.h>
+
+#include "components/bounding_box_component/BoundingBoxComponent.h"
+#include "components/graphics_component/GraphicsComponent.h"
+#include "entities/EntitiesManager.h"
+#include "states/GameState.h"
+#include "components/camera_component/CameraComponent.h"
+#include "handlers/animation/AnimationHandler.h"
+
+namespace Project::Components {
+
+}

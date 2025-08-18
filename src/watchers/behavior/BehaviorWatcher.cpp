a95e6d74f1a195ac6a4d29c80a10a6026a0c27f0
rn-santos227 <rn_santos227@yahoo.com>
2025-08-18 20:14:44 +0800

    added constructor in behavior watcher source file

--- src/watchers/behavior/BehaviorWatcher.cpp
+++ src/watchers/behavior/BehaviorWatcher.cpp
@@ -3,5 +3,6 @@
 #include <utility>
 
 namespace Project::Watchers {
-
+  BehaviorWatcher::BehaviorWatcher(Condition condition, Command command)
+  : condition(std::move(condition)), command(std::move(command)) {}
 }

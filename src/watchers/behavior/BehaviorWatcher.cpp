452f892815690bd88ce417c750c2d0c77215e850
rn-santos227 <rn_santos227@yahoo.com>
2025-08-18 20:15:05 +0800

    added check function in behavior watcher source file

--- src/watchers/behavior/BehaviorWatcher.cpp
+++ src/watchers/behavior/BehaviorWatcher.cpp
@@ -5,4 +5,10 @@
 namespace Project::Watchers {
   BehaviorWatcher::BehaviorWatcher(Condition condition, Command command)
   : condition(std::move(condition)), command(std::move(command)) {}
+
+  void BehaviorWatcher::check() {
+    if (condition && condition()) {
+      if (command) command();
+    }
+  }
 }

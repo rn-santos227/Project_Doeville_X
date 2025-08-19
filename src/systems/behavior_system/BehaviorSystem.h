8f712cd1b18bf68d6b466398d468ed036f2b7fcc
rn-santos227 <rn_santos227@yahoo.com>
2025-08-19 20:43:49 +0800

    updated behavior system header file

--- src/systems/behavior_system/BehaviorSystem.h
+++ src/systems/behavior_system/BehaviorSystem.h
@@ -1,14 +1,20 @@
 #ifndef BEHAVIOR_SYSTEM_H
 #define BEHAVIOR_SYSTEM_H
 
+#include <vector>
+
 #include "interfaces/update_interface/Updatable.h"
-#include "utilities/profiler/Profiler.h"
-#include "libraries/constants/ProfileConstants.h"
+
+namespace Project { namespace Components { class BehaviorComponent; } }
 
 namespace Project::Systems {
   class BehaviorSystem : public Project::Interfaces::Updatable {
   public:
     void update(float deltaTime) override;
+    
+    void add(Project::Components::BehaviorComponent* component);
+    void remove(Project::Components::BehaviorComponent* component);
+    void clear();
   };
 }
 

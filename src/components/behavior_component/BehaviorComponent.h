4658585efe4a765f34f964b792751ce803db5673
rn-santos227 <rn_santos227@yahoo.com>
2025-08-19 20:19:36 +0800

    updated behavior handler header file

--- src/components/behavior_component/BehaviorComponent.h
+++ src/components/behavior_component/BehaviorComponent.h
@@ -4,6 +4,14 @@
 #include <string>
 
 #include "components/BaseComponent.h"
+#include "handlers/behavior/BehaviorHandler.h"
 
+namespace Project { namespace Entities { class Entity; } }
+
+namespace Project::Components {
+  class BehaviorComponent : public BaseComponent {
+
+  };
+}
 
 #endif

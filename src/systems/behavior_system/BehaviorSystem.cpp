682e2bec463a4f4f4ed6ae45ffa049925b8f06ff
rn-santos227 <rn_santos227@yahoo.com>
2025-08-19 20:47:43 +0800

    updated behavior system source file

--- src/systems/behavior_system/BehaviorSystem.cpp
+++ src/systems/behavior_system/BehaviorSystem.cpp
@@ -1,8 +1,33 @@
+
 #include "BehaviorSystem.h"
 
+#include <algorithm>
+
+#include "components/behavior_component/BehaviorComponent.h"
+#include "libraries/constants/ProfileConstants.h"
+#include "utilities/profiler/Profiler.h"
+
 namespace Project::Systems {
-  void BehaviorSystem::update(float /*deltaTime*/) {
+  using Project::Components::BehaviorComponent;
+
+  void BehaviorSystem::update(float deltaTime) {
     PROFILE_SCOPE(Project::Libraries::Constants::BEHAVIOR_PROFILE);
-    // Placeholder behavior update job
+    for (auto* comp : components) {
+      if (comp && comp->isActive()) {
+        comp->update(deltaTime);
+      }
+    }
+  }
+
+  void BehaviorSystem::add(BehaviorComponent* component) {
+    if (component) components.push_back(component);
+  }
+
+  void BehaviorSystem::remove(BehaviorComponent* component) {
+    components.erase(std::remove(components.begin(), components.end(), component), components.end());
+  }
+
+  void BehaviorSystem::clear() {
+    components.clear();
   }
 }

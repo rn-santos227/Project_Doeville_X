9d70e32b6e6b4f0d8be617fa929d5e4e601f798a
rn-santos227 <rn_santos227@yahoo.com>
2025-08-19 20:22:32 +0800

    created behavior component source file

---
+++ src/components/behavior_component/BehaviorComponent.cpp
@@ -0,0 +1,9 @@
+#include "BehaviorComponent.h"
+
+#include "libraries/constants/Constants.h"
+#include "libraries/keys/Keys.h"
+
+namespace Project::Components {
+  BehaviorComponent::BehaviorComponent(Project::Utilities::LogsManager& logsManager)
+  : BaseComponent(logsManager), handler(logsManager) {}
+}

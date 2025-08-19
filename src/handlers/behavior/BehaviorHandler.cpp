3a9ed9e4c384d6b7c7de7410ebe1c24c53bed300
rn-santos227 <rn_santos227@yahoo.com>
2025-08-19 20:17:42 +0800

    added add watcher function in behavior handler source file

--- src/handlers/behavior/BehaviorHandler.cpp
+++ src/handlers/behavior/BehaviorHandler.cpp
@@ -1,12 +1,19 @@
 #include "BehaviorHandler.h"
 
 namespace Project::Handlers {
-  BehaviorHandler::BehaviorHandler(Project::Utilities::LogsManager& logsManager)
+  using Project::Utilities::LogsManager;
+  using Project::Watchers::BehaviorWatcher;
+
+  BehaviorHandler::BehaviorHandler(LogsManager& logsManager)
   : logsManager(logsManager) {}
 
   void BehaviorHandler::update() {
     for (auto& watcher : watchers) {
       watcher.check();
     }
+  }
+
+  void BehaviorHandler::addWatcher(const BehaviorWatcher& watcher) {
+    watchers.push_back(watcher);
   }
 }

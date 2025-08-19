51642cb11581a57a20f331b0c02ce4bdc2d3dfdd
rn-santos227 <rn_santos227@yahoo.com>
2025-08-19 20:16:34 +0800

    added update function in behavior handler source file

--- src/handlers/behavior/BehaviorHandler.cpp
+++ src/handlers/behavior/BehaviorHandler.cpp
@@ -3,4 +3,10 @@
 namespace Project::Handlers {
   BehaviorHandler::BehaviorHandler(Project::Utilities::LogsManager& logsManager)
   : logsManager(logsManager) {}
+
+  void BehaviorHandler::update() {
+    for (auto& watcher : watchers) {
+      watcher.check();
+    }
+  }
 }

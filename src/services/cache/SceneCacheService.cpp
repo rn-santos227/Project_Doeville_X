6ae57cc6a5a85bd3f16683b1cb9883f0756f9594
rn-santos227 <rn_santos227@yahoo.com>
2025-08-20 19:38:14 +0800

    created scene cache service source file

---
+++ src/services/cache/SceneCacheService.cpp
@@ -0,0 +1,11 @@
+#include "SceneCacheService.h"
+
+#include <chrono>
+#include <cstring>
+
+#include "libraries/constants/PathConstants.h"
+#include "utilities/profiler/CacheProfiler.h"
+
+namespace Project::Services {
+
+}

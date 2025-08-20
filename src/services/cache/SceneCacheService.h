efb7ae4170479c37abebd15ca3182aea10fab504
rn-santos227 <rn_santos227@yahoo.com>
2025-08-20 19:22:54 +0800

    created scene cache service header file

---
+++ src/services/cache/SceneCacheService.h
@@ -0,0 +1,19 @@
+#ifndef SCENE_CACHE_SERVICE_H
+#define SCENE_CACHE_SERVICE_H
+
+#include <cstdint>
+#include <functional>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "utilities/binary_cache/BinaryFileCache.h"
+#include "utilities/logs_manager/LogsManager.h"
+
+namespace Project::Services {
+  class SceneCacheService {
+
+  };
+}
+
+#endif

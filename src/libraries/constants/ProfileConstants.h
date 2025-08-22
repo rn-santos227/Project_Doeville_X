c5abcaf81f4e2c9fa3b9b8236e2a1d3dfe9751a4
rn-santos227 <rn_santos227@yahoo.com>
2025-08-22 21:17:37 +0800

    updated profile constants header file

--- src/libraries/constants/ProfileConstants.h
+++ src/libraries/constants/ProfileConstants.h
@@ -8,6 +8,10 @@ namespace Project::Libraries::Constants {
   constexpr const char* MOTION_PROFILE = "MotionSystem::update";
   constexpr const char* PHYSICS_PROFILE = "PhysicsSystem::update";
   constexpr const char* RENDER_PROFILE = "RenderSystem::render";
+
+  constexpr const char* RENDER_SCOPE = "render";
+  constexpr const char* POST_PROCESS_SCOPE = "postprocess";
+  constexpr const char* PARTICLE_SCOPE = "particles";
 }
 
 #endif

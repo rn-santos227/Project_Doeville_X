ebd483b6bca53b29109078df151a75e5a8c1a72c
rn-santos227 <rn_santos227@yahoo.com>
2025-08-23 18:08:12 +0800

    created open gl renderer header file in platform folder

---
+++ src/platform/renderer/OpenGLRenderer.h
@@ -0,0 +1,12 @@
+#ifndef PLATFORM_OPENGL_RENDERER_H
+#define PLATFORM_OPENGL_RENDERER_H
+
+#include "RendererAPI.h"
+
+namespace Project::Platform {
+  class OpenGLRenderer : public RendererAPI {
+
+  };
+}
+
+#endif

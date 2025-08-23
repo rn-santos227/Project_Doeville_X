2ccd74d9e7cc58d62e572a24bb5942582435b3e1
rn-santos227 <rn_santos227@yahoo.com>
2025-08-23 18:01:02 +0800

    updated renderer api header file in platform folder

--- src/platform/renderer/RendererAPI.h
+++ src/platform/renderer/RendererAPI.h
@@ -8,6 +8,8 @@ namespace Project::Platform {
   public:
     virtual ~RendererAPI() = default;
     virtual bool init() = 0;
+    virtual void clear() = 0;
+    virtual void present() = 0;
   };
 }
 

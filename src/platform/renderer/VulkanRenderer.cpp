7e09400b77209fa7cc719ee8e0ba03168dfed36b
rn-santos227 <rn_santos227@yahoo.com>
2025-08-23 18:06:31 +0800

    created vulkan renderer source file in platform folder

---
+++ src/platform/renderer/VulkanRenderer.cpp
@@ -0,0 +1,7 @@
+#include "VulkanRenderer.h"
+
+namespace Project::Platform {
+  bool VulkanRenderer::init() { return true; }
+  void VulkanRenderer::clear() {}
+  void VulkanRenderer::present() {}
+}

96ed63b19217c5dc76081d233c795b88065e68c7
rn-santos227 <rn_santos227@yahoo.com>
2025-08-25 01:28:41 +0800

    created endian helper header file in serialization helper folder

---
+++ src/helpers/serialization/EndianHelper.h
@@ -0,0 +1,17 @@
+#ifndef ENDIAN_HELPER_H
+#define ENDIAN_HELPER_H
+
+#include <algorithm>
+#include <array>
+#include <cstdint>
+#include <cstring>
+#include <istream>
+#include <type_traits>
+#include <ostream>
+#include <vector>
+
+namespace Project::Helpers {
+
+}
+
+#endif

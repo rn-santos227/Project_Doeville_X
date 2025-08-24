074cbcc1f9e9b2a4b09f68f396a906791d49ca87
rn-santos227 <rn_santos227@yahoo.com>
2025-08-24 19:56:12 +0800

    updated compression utils source and header files

--- src/utilities/compression/CompressionUtils.h
+++ src/utilities/compression/CompressionUtils.h
@@ -8,6 +8,7 @@ namespace Project::Utilities {
   struct CompressionUtils {
     static bool compress(const unsigned char* src, size_t srcSize, std::vector<unsigned char>& dest);
     static bool decompress(const unsigned char* src, size_t srcSize, std::vector<unsigned char>& dest);
+    static bool decompressTo(const unsigned char* src, size_t srcSize, unsigned char* dest, size_t destSize, size_t& outSize);
   };
 }
 

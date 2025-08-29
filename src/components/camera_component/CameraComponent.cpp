416c73479ed1a90c5dfdac8bbe426c4a038d4a9b
rn-santos227 <rn_santos227@yahoo.com>
2025-08-29 22:41:19 +0800

    updated zoom out function in camera component source file

--- src/components/camera_component/CameraComponent.cpp
+++ src/components/camera_component/CameraComponent.cpp
@@ -214,7 +214,7 @@ namespace Project::Components {
   }
 
   void CameraComponent::zoomOut(float amount) {
-    targetZoom -= amount;
+    targetZoom = std::clamp(targetZoom - amount, Constants::DEFAULT_CAMERA_MIN_ZOOM, Constants::DEFAULT_CAMERA_MAX_ZOOM);
   }
 
   void CameraComponent::setRotation(float angle) {

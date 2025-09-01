911dda1e695bf09cb1ccc6feed33d4d492b14cbc
rn-santos227 <rn_santos227@yahoo.com>
2025-09-01 15:38:23 +0800

    updated game state data header file

--- src/states/GameStateData.h
+++ src/states/GameStateData.h
@@ -1,11 +1,13 @@
 #ifndef GAME_STATE_DATA_H
 #define GAME_STATE_DATA_H
 
+#include "GameStateCategory.h"
+#include "DimensionMode.h"
+
 #include <string>
 #include <SDL2/SDL.h>
 
-#include "GameStateCategory.h"
-#include "DimensionMode.h"
+#include "libraries/constants/TimeConstants.h"
 
 namespace Project::States {
   struct GameStateData {
@@ -16,7 +18,9 @@ namespace Project::States {
     SDL_Rect mapRect{0, 0, 0, 0};
     
     float darkness = 0.0f;
-
+    float timeOfDay = Project::Libraries::Constants::DEFAULT_TIME_OF_DAY_MINUTES;
+    float dayLapseSeconds = Project::Libraries::Constants::DEFAULT_DAY_LAPSE_SECONDS;
+    
     bool active = false;
     bool initialized = false;
   };

5f840dc125004a087dbeb32e8f3fe3612cd51c2f
rn-santos227 <rn_santos227@yahoo.com>
2025-09-02 21:08:51 +0800

    updated frames counter header file

--- src/utilities/frames_counter/FramesCounter.h
+++ src/utilities/frames_counter/FramesCounter.h
@@ -14,12 +14,15 @@ namespace Project::Utilities {
     void update();
 
     double getDeltaTime() const;
+    double getTotalTime() const;
+    
     int getFPS() const; 
 
   private:
     Uint64 lastTime;
     Uint64 frequency;
     Uint64 startFpsTime = 0;
+    Uint64 startTime;
 
     double deltaTime;
 

37ecf6be87c03bec1c984968b495b590af6aa062
rn-santos227 <rn_santos227@yahoo.com>
2025-09-05 20:58:27 +0800

    created network watcher header file

---
+++ src/watchers/network/NetworkWatcher.h
@@ -0,0 +1,11 @@
+#ifndef NETWORK_WATCHER_H
+#define NETWORK_WATCHER_H
+
+#include <atomic>
+#include <thread>
+
+#include "services/network/NetworkService.h"
+#include "utilities/logs_manager/LogsManager.h"
+
+
+#endif

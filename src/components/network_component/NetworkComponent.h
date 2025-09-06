f297cbfadeb54a709133efa0a409caae2a51ed43
rn-santos227 <rn_santos227@yahoo.com>
2025-09-06 19:46:04 +0800

    created network component header file

---
+++ src/components/network_component/NetworkComponent.h
@@ -0,0 +1,11 @@
+#ifndef NETWORK_COMPONENT_H
+#define NETWORK_COMPONENT_H
+
+#include "NetworkData.h"
+
+#include "components/BaseComponent.h"
+#include "services/network/NetworkService.h"
+
+
+
+#endif

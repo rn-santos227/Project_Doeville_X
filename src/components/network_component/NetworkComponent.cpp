c36498aefb56778175d551777ebcad4e87ae2162
rn-santos227 <rn_santos227@yahoo.com>
2025-09-06 19:57:12 +0800

    created network component source file

---
+++ src/components/network_component/NetworkComponent.cpp
@@ -0,0 +1,12 @@
+#include "NetworkComponent.h"
+
+#include <sstream>
+
+#include "entities/Entity.h"
+#include "libraries/keys/Keys.h"
+#include "utilities/lua_state_wrapper/LuaStateWrapper.h"
+#include "watchers/network/NetworkWatcher.h"
+
+namespace Project::Components {
+
+}

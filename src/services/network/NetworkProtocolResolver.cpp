b98fa2206d1567c8ee471c3b68ab4adc118d57b1
rn-santos227 <rn_santos227@yahoo.com>
2025-09-07 20:01:27 +0800

    updated network protocol resolver source file

--- src/services/network/NetworkProtocolResolver.cpp
+++ src/services/network/NetworkProtocolResolver.cpp
@@ -11,7 +11,9 @@ namespace Project::Services {
 
   NetworkProtocol NetworkProtocolResolver::resolve(std::string_view name) {
     static constexpr std::array<std::pair<std::string_view, NetworkProtocol>, Protocols::PROTOCOL_NAME_ALIAS_COUNT> map{{
-
+      {Protocols::HTTP, NetworkProtocol::HTTP},
+      {Protocols::WEBSOCKET, NetworkProtocol::WebSocket},
+      {Protocols::P2P, NetworkProtocol::P2P},
     }};
   }
 }

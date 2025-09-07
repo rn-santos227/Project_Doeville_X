10003d02aabff1feeb33387db19b59b4c278e126
rn-santos227 <rn_santos227@yahoo.com>
2025-09-07 18:39:19 +0800

    updated modal type resolver source file

--- src/components/modal_component/ModalTypeResolver.cpp
+++ src/components/modal_component/ModalTypeResolver.cpp
@@ -4,15 +4,13 @@
 #include <string_view>
 
 #include "libraries/categories/ModalCategories.h"
-#include "libraries/constants/NumericConstants.h"
 #include "utilities/string/StringUtils.h"
 
 namespace Project::Components {
   namespace Modals = Project::Libraries::Categories::Modals;
-  namespace Constants = Project::Libraries::Constants;
 
   ModalType ModalTypeResolver::resolve(std::string_view name) {
-    static constexpr std::array<std::pair<std::string_view, ModalType>, Constants::MODAL_NAME_ALIAS_COUNT> map{{
+    static constexpr std::array<std::pair<std::string_view, ModalType>, Modals::MODAL_NAME_ALIAS_COUNT> map{{
       {Modals::NOTIFICATION, ModalType::NOTIFICATION},
       {Modals::PROMPT, ModalType::PROMPT},
       {Modals::OPTIONS, ModalType::OPTIONS},
--- src/libraries/categories/ModalCategories.h
+++ src/libraries/categories/ModalCategories.h
@@ -3,6 +3,8 @@
 
 namespace Project::Libraries::Categories {
   namespace Modals {
+    constexpr size_t MODAL_NAME_ALIAS_COUNT = 4;
+
     constexpr const char* NOTIFICATION = "NOTIFICATION";
     constexpr const char* PROMPT = "PROMPT";
     constexpr const char* OPTIONS = "OPTIONS";

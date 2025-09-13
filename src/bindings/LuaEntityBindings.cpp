b7569760674e7cf6096a5b1b41f04a5ce71145f1
rn-santos227 <rn_santos227@yahoo.com>
2025-09-13 19:00:46 +0800

    updated multiply numeric value function in lue entity bindings source file

--- src/bindings/LuaEntityBindings.cpp
+++ src/bindings/LuaEntityBindings.cpp
@@ -525,5 +525,10 @@ namespace Project::Bindings::LuaBindings {
     if (!manager || !name || !key) {
       return 0;
     }
+    auto entity = manager->getEntity(name);
+    if (!entity && manager->getGameState()) {
+      entity = manager->getGameState()->findEntity(name);
+    }
+
   }
 }

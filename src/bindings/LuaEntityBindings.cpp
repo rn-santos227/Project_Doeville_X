206ce2a9fb3684316fa3b35ffb1e0c47e2036a54
rn-santos227 <rn_santos227@yahoo.com>
2025-09-13 19:03:53 +0800

    updated divide numeric value function in lue entity bindings source file

--- src/bindings/LuaEntityBindings.cpp
+++ src/bindings/LuaEntityBindings.cpp
@@ -543,5 +543,8 @@ namespace Project::Bindings::LuaBindings {
     EntitiesManager* manager = static_cast<EntitiesManager*>(lua_touserdata(L, lua_upvalueindex(Constants::INDEX_ONE)));
     const char* name = luaL_checkstring(L, Constants::INDEX_ONE);
     const char* key = luaL_checkstring(L, Constants::INDEX_TWO);
+    if (!manager || !name || !key) {
+      return 0;
+    }
   }
 }

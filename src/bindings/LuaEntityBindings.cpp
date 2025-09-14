0e540367d4c0fffe5bb7d18b17388e95b7eb3de8
rn-santos227 <rn_santos227@yahoo.com>
2025-09-14 18:16:56 +0800

    updated get numeric value in lua entity bindings source file

--- src/bindings/LuaEntityBindings.cpp
+++ src/bindings/LuaEntityBindings.cpp
@@ -590,5 +590,9 @@ namespace Project::Bindings::LuaBindings {
     EntitiesManager* manager = static_cast<EntitiesManager*>(lua_touserdata(L, lua_upvalueindex(Constants::INDEX_ONE)));
     const char* name = luaL_checkstring(L, Constants::INDEX_ONE);
     const char* key = luaL_checkstring(L, Constants::INDEX_TWO);
+    if (!manager || !name || !key) {
+      lua_pushnil(L);
+      return Constants::INDEX_ONE;
+    }
   }
 }

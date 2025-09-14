1a9dc0f8e71b263798ac7a21c7810c45f952ec4c
rn-santos227 <rn_santos227@yahoo.com>
2025-09-14 18:20:45 +0800

    updated lua break entity function in lua entity bindings source file

--- src/bindings/LuaEntityBindings.cpp
+++ src/bindings/LuaEntityBindings.cpp
@@ -633,6 +633,7 @@ namespace Project::Bindings::LuaBindings {
   }
 
   int lua_brakeEntity(lua_State* L) {
-
+    EntitiesManager* manager = static_cast<EntitiesManager*>(lua_touserdata(L, lua_upvalueindex(Constants::INDEX_ONE)));
+    const char* name = luaL_checkstring(L, Constants::INDEX_ONE);
   }
 }

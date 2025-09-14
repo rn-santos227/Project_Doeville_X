933d383ecbbf3c0c20d19372c617024e2efac4d8
rn-santos227 <rn_santos227@yahoo.com>
2025-09-14 18:19:19 +0800

    updated lua stop timer function in lua entity bindings source file

--- src/bindings/LuaEntityBindings.cpp
+++ src/bindings/LuaEntityBindings.cpp
@@ -619,5 +619,9 @@ namespace Project::Bindings::LuaBindings {
     if (!manager || !name) {
       return 0;
     }
+    auto entity = manager->getEntity(name);
+    if (!entity && manager->getGameState()) {
+      entity = manager->getGameState()->findEntity(name);
+    }
   }
 }

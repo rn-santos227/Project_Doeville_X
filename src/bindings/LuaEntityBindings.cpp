3e94c768de9b46f22c71b8d86d874a42266b6aeb
rn-santos227 <rn_santos227@yahoo.com>
2025-09-14 18:19:08 +0800

    updated lua stop timer function in lua entity bindings source file

--- src/bindings/LuaEntityBindings.cpp
+++ src/bindings/LuaEntityBindings.cpp
@@ -616,5 +616,8 @@ namespace Project::Bindings::LuaBindings {
   int lua_stopTimer(lua_State* L) {
     EntitiesManager* manager = static_cast<EntitiesManager*>(lua_touserdata(L, lua_upvalueindex(Constants::INDEX_ONE)));
     const char* name = luaL_checkstring(L, Constants::INDEX_ONE);
+    if (!manager || !name) {
+      return 0;
+    }
   }
 }
